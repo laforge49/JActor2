cc8e2247173a062b2c847f5ae22dbabdd39d381d
Bill La Forge <laforge49@gmail.com>
2013-03-30 16:48:44 +0530

    Updated the javadoc for _Request

--- src/main/java/org/agilewiki/pactor/_Request.java
+++ src/main/java/org/agilewiki/pactor/_Request.java
@@ -3,6 +3,12 @@ package org.agilewiki.pactor;
 /**
  * A _Request object is passed to another actor's mailbox for execution,
  * just as a Runnable is passed by SwingUtilities.invokeLater for execution on the UI thread.
+ * <p>
+ * Requests may either be bound to a target mailbox or unbound.
+ * In the case of unbound requests, the target actor is specified when the _Request
+ * is passed, and the target mailbox is determined
+ * by calling getMailbox() on the target actor.
+ * </p>
  *
  * @param <RESPONSE_TYPE>     the type of response to be returned.
  * @param <TARGET_ACTOR_TYPE> For UnboundRequest's, this is the class of the target actor.
