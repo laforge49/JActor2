dc25875d9b2ef0c5e9dcace7ba2aa65d35f2c0f6
Bill La Forge <laforge49@gmail.com>
2013-03-30 15:20:18 +0530

    Updated Javadoc for ResponseProcessor

--- src/main/java/org/agilewiki/pactor/ResponseProcessor.java
+++ src/main/java/org/agilewiki/pactor/ResponseProcessor.java
@@ -1,6 +1,23 @@
 package org.agilewiki.pactor;
 
+/**
+ * A callback sent with a _Request as a means of returning a response,
+ * or a callback passed to the _Request.processRequest method.
+ *
+ * @param <RESPONSE_TYPE> the type of response.
+ */
 public interface ResponseProcessor<RESPONSE_TYPE> {
+    /**
+     * The processResponse method accepts the response of a request.
+     * <p>
+     * In the case of a ResponseProcessor object sent with a _Request,
+     * this method need not be thread-safe, as it
+     * is always invoked from the same light-weight thread that sent the
+     * _Request and ResponseProcessor objects.
+     * </p>
+     *
+     * @param response the response to a request.
+     */
     public void processResponse(final RESPONSE_TYPE response)
             throws Exception;
 }
