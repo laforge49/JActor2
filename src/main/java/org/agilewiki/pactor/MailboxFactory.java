a7a768d992d42a0ec89b74e55ab4cf5542de1a74
Bill La Forge <laforge49@gmail.com>
2013-03-31 13:21:35 +0530

    Updated MailboxFactory javadocs.

--- src/main/java/org/agilewiki/pactor/MailboxFactory.java
+++ src/main/java/org/agilewiki/pactor/MailboxFactory.java
@@ -1,47 +1,70 @@
-/**
- *
- */
 package org.agilewiki.pactor;
 
 /**
- * <p>
- * The MailboxFactory is the factory as the name suggests for the MailBoxes to be used with the PActor. In addition to
- * creation of the Mailboxes it also encapsulates the threads( threadpool) which would process the Requests added to
- * the mailbox in asynchronous mode.
- * </p>
+ * MailboxFactory creates different types of mailboxes, including thread bound mailboxes.
+ * This class is also responsible for closing everything down and managing a list of
+ * auto closables to be called when MailboxFactory.close() is called.
  */
 public interface MailboxFactory extends AutoCloseable {
 
-    /** Creates a Mailbox with a default message queue
-     * with commandeering and message buffering enabled. */
+    /**
+     * Creates a Mailbox with a both commandeering and message buffering enabled.
+     *
+     * @return A new mailbox.
+     */
     Mailbox createMailbox();
 
-    /** Creates a Mailbox with a default message queue
-     * with message buffering enabled. */
+    /**
+     * Creates a Mailbox with commandeering enabled.
+     *
+     * @param _disableCommandeering True when commandeering is to be disabled.
+     * @return A new mailbox.
+     */
     Mailbox createMailbox(final boolean _disableCommandeering);
 
-    /** Creates a Mailbox with a default message queue. */
+    /**
+     * Creates a Mailbox.
+     *
+     * @param _disableCommandeering True when commandeering is to be disabled.
+     * @param _disableMessageBuffering True when message buffering is to be disabled.
+     * @return A new mailbox.
+     */
     Mailbox createMailbox(final boolean _disableCommandeering,
                           final boolean _disableMessageBuffering);
 
     /**
-     * Create a mailbox that runs on the current thread.
+     * Create a mailbox that runs on an existing thread.
      * <p>
-     * When messageProcessor.run will typically us SwingUtilities.invokeLater(mailbox)
-     * to process the pending messages.
+     * The _messageProcessor.run method typically will call
+     * SwingUtilities.invokeLater(mailbox) to process pending messages on the UI thread.
      * </p>
      *
-     *
-     * @param _messageProcessor The run method is called when there are messages t be processed.
+     * @param _messageProcessor The run method is called when there are messages
+     *                          to be processed.
      * @return The thread bounded mailbox.
      */
     Mailbox createThreadBoundMailbox(final Runnable _messageProcessor);
 
-    /** Adds a closeable, to close when the MailboxFactory closes down. */
+    /**
+     * Adds an auto closeable, to be closed when the MailboxFactory closes.
+     *
+     * @param closeable The autoclosable to be added to the list.
+     * @return True, if the list was updated.
+     */
     boolean addAutoClosable(final AutoCloseable closeable);
 
+    /**
+     * Remove an auto closeable from the list of closables.
+     *
+     * @param closeable The autoclosable to be removed from the list.
+     * @return True, if the list was updated.
+     */
     boolean removeAutoClosable(final AutoCloseable closeable);
 
-    /** Returns true, if close() has been called already. */
+    /**
+     * Returns true if close() has been called already.
+     *
+     * @return true if close() has already been called.
+     */
     boolean isClosing();
 }
