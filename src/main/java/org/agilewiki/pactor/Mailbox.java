3e108aef7ce615a26cd15b6a6c58df12bfd7db67
Bill La Forge <laforge49@gmail.com>
2013-04-02 19:08:29 +0530

    Cleaned up the Mailbox javadoc.

--- src/main/java/org/agilewiki/pactor/Mailbox.java
+++ src/main/java/org/agilewiki/pactor/Mailbox.java
@@ -21,16 +21,22 @@ public interface Mailbox extends Runnable, _Mailbox {
      * processing and all buffered results/exceptions to their source
      * mailbox. For results/exceptions originating from a call, the calling thread
      * is unblocked and the results returned or the exception thrown.
-     * @return
+     * <p>
+     * The flush method is called when the last enqueued request/result is processed.
+     * However, there may be special cases where an explicit flush may be needed.
+     * </p>
+     *
+     * @return True when one or more buffered request/result was delivered.
      */
     boolean flush() throws Exception;
 
     /**
      * Replace the current ExceptionHandler with another.
+     *
      * @param exceptionHandler The exception handler to be used now.
      *                         May be null if the default exception handler is to be used.
      * @return The exception handler that was previously in effect, or null if the
-     * default exception handler was in effect.
+     *         default exception handler was in effect.
      */
     ExceptionHandler setExceptionHandler(final ExceptionHandler exceptionHandler);
 }
