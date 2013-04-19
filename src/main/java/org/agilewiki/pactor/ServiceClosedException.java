892995aac331be23654ccfce9f06dfe97b0284af
Bill La Forge <laforge49@gmail.com>
2013-04-19 08:32:58 +0530

    new ServiceClosedException

---
+++ src/main/java/org/agilewiki/pactor/ServiceClosedException.java
@@ -0,0 +1,7 @@
+package org.agilewiki.pactor;
+
+/**
+ * This exception may be thrown when calling or sending a Request and the MailboxFactory is closed.
+ */
+public class ServiceClosedException extends Exception {
+}
