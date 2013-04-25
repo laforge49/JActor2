53e607f2dd0f54065370ae67b06b474355608b75
Bill La Forge <laforge49@gmail.com>
2013-04-25 18:08:21 +0530

    ported durable.Util

--- pactor-durable/src/main/java/org/agilewiki/pactor/durable/Util.java
+++ pactor-durable/src/main/java/org/agilewiki/pactor/durable/Util.java
@@ -8,6 +8,31 @@ import org.agilewiki.pautil.Ancestor;
 
 public class Util {
 
+    /**
+     * Size of a boolean in bytes.
+     */
+    public final static int BOOLEAN_LENGTH = 1;
+
+    /**
+     * Size of an int in bytes.
+     */
+    public final static int INT_LENGTH = 4;
+
+    /**
+     * Size of a long in bytes.
+     */
+    public final static int LONG_LENGTH = 8;
+
+    /**
+     * Size of an float in bytes.
+     */
+    public final static int FLOAT_LENGTH = 4;
+
+    /**
+     * Size of an double in bytes.
+     */
+    public final static int DOUBLE_LENGTH = 8;
+
     public static FactoryLocator getFactoryLocator(final Properties _properties) {
         return (FactoryLocator) _properties.getProperty("factoryLocator");
     }
@@ -24,13 +49,57 @@ public class Util {
         return getFactoryLocator(_actor.getMailbox());
     }
 
-    public static PASerializable newJid(Actor actor, String jidType, Mailbox mailbox, Ancestor parent)
+    public static PASerializable newJid(final Actor _actor,
+                                        final String _factoryName,
+                                        final Mailbox _mailbox,
+                                        final Ancestor _parent)
             throws Exception {
-        return newJid(getFactoryLocator(actor), jidType, mailbox, parent);
+        return newJid(getFactoryLocator(_actor), _factoryName, _mailbox, _parent);
     }
 
-    public static PASerializable newJid(FactoryLocator factoryLocator, String jidType, Mailbox mailbox, Ancestor parent)
+    public static PASerializable newJid(final FactoryLocator _factoryLocator,
+                                        final String _factoryName,
+                                        final Mailbox _mailbox,
+                                        final Ancestor _parent)
             throws Exception {
-        return factoryLocator.newJid(jidType, mailbox, parent);
+        return _factoryLocator.newJid(_factoryName, _mailbox, _parent);
+    }
+
+    public static Factory getActorFactory(final Actor _actor, final String _factoryName)
+            throws Exception {
+        return getActorFactory(getFactoryLocator(_actor), _factoryName);
+    }
+
+    public static Factory getActorFactory(final FactoryLocator _factoryLocator, final String _factoryName)
+            throws Exception {
+        if (_factoryLocator == null)
+            throw new IllegalArgumentException("Unknown jid type: " + _factoryName);
+        return _factoryLocator.getFactory(_factoryName);
+    }
+
+    /**
+     * Returns the number of bytes needed to write a string.
+     *
+     * @param _length The number of characters in the string.
+     * @return The size in bytes.
+     */
+    public final static int stringLength(final int _length) {
+        if (_length == -1)
+            return INT_LENGTH;
+        if (_length > -1)
+            return INT_LENGTH + 2 * _length;
+        throw new IllegalArgumentException("invalid string length: " + _length);
+    }
+
+    /**
+     * Returns the number of bytes needed to write a string.
+     *
+     * @param _s The string.
+     * @return The size in bytes.
+     */
+    public final static int stringLength(final String _s) {
+        if (_s == null)
+            return INT_LENGTH;
+        return stringLength(_s.length());
     }
 }
