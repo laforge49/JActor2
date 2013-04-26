4e24cf2e822387891b850f4dec681dfc94eca50c
Bill La Forge <laforge49@gmail.com>
2013-04-26 14:13:01 +0530

    ported DurableImpl and AppFactory

---
+++ pactor-durable/src/main/java/org/agilewiki/pactor/durable/AppFactory.java
@@ -0,0 +1,59 @@
+package org.agilewiki.pactor.durable;
+
+import org.agilewiki.pactor.Mailbox;
+import org.agilewiki.pactor.durable.impl.DurableImpl;
+import org.agilewiki.pactor.durable.impl.FactoryImpl;
+import org.agilewiki.pautil.Ancestor;
+
+/**
+ * Creates DurableImpl objects.
+ */
+public abstract class AppFactory extends FactoryImpl {
+    private Factory[] tupleFactories;
+    private String[] jidTypes;
+
+    public AppFactory(String subActorType) {
+        super(subActorType);
+        this.jidTypes = new String[0];
+    }
+
+    /**
+     * Create a JLPCActorFactory.
+     *
+     * @param subJidType The jid type.
+     * @param jidTypes   The element types.
+     */
+    public AppFactory(String subJidType, String... jidTypes) {
+        super(subJidType);
+        this.jidTypes = jidTypes;
+    }
+
+    @Override
+    abstract protected App instantiateActor();
+
+    /**
+     * Create and configure an actor.
+     *
+     * @param mailbox The mailbox of the new actor.
+     * @param parent  The parent of the new actor.
+     * @return The new actor.
+     */
+    @Override
+    public App newSerializable(Mailbox mailbox, Ancestor parent)
+            throws Exception {
+        App a = instantiateActor();
+        DurableImpl tj = new DurableImpl();
+        a.setDurable(tj);
+        tj.initialize(mailbox, parent, this);
+        FactoryLocator fl = getFactoryLocator();
+        Factory[] afs = new FactoryImpl[jidTypes.length];
+        int i = 0;
+        while (i < jidTypes.length) {
+            afs[i] = fl.getFactory(jidTypes[i]);
+            i += 1;
+        }
+        tupleFactories = afs;
+        tj.tupleFactories = tupleFactories;
+        return a;
+    }
+}
---
+++ pactor-durable/src/main/java/org/agilewiki/pactor/durable/impl/DurableImpl.java
@@ -0,0 +1,198 @@
+package org.agilewiki.pactor.durable.impl;
+
+import org.agilewiki.pactor.durable.*;
+
+/**
+ * A base class for applications, DurableImpl provides a durable tuple without an external interface.
+ */
+public class DurableImpl extends IncDesImpl implements Durable {
+    /**
+     * The size of the serialized data (exclusive of its length header).
+     */
+    private int _len;
+
+    /**
+     * An array of jid factories, one for each element in the tuple.
+     */
+    public Factory[] tupleFactories;
+
+    /**
+     * A tuple of actors.
+     */
+    protected PASerializable[] tuple;
+
+    /**
+     * Returns the element factories.
+     *
+     * @return An array of element factories.
+     */
+    protected Factory[] getTupleFactories()
+            throws Exception {
+        if (tupleFactories != null)
+            return tupleFactories;
+        throw new IllegalStateException("tupleFactories is null");
+    }
+
+    @Override
+    public void _iSetBytes(int i, byte[] bytes)
+            throws Exception {
+        _initialize();
+        PASerializable elementJid = createSubordinate(tupleFactories[i], bytes);
+        PASerializable oldElementJid = _iGet(i);
+        ((IncDesImpl) oldElementJid.getDurable()).setContainerJid(null);
+        tuple[i] = elementJid;
+        change(elementJid.getDurable().getSerializedLength() -
+                oldElementJid.getDurable().getSerializedLength());
+    }
+
+    @Override
+    public int _size()
+            throws Exception {
+        return getTupleFactories().length;
+    }
+
+    @Override
+    public PASerializable _iGet(int i) throws Exception {
+        _initialize();
+        if (i < 0)
+            i += _size();
+        if (i < 0 || i >= _size())
+            return null;
+        return tuple[i];
+    }
+
+    @Override
+    public PASerializable _resolvePathname(String pathname)
+            throws Exception {
+        if (pathname.length() == 0) {
+            throw new IllegalArgumentException("empty string");
+        }
+        int s = pathname.indexOf("/");
+        if (s == -1)
+            s = pathname.length();
+        if (s == 0)
+            throw new IllegalArgumentException("pathname " + pathname);
+        String ns = pathname.substring(0, s);
+        int n = 0;
+        try {
+            n = Integer.parseInt(ns);
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("pathname " + pathname);
+        }
+        if (n < 0 || n >= _size())
+            throw new IllegalArgumentException("pathname " + pathname);
+        PASerializable jid = _iGet(n);
+        if (s == pathname.length())
+            return jid;
+        return jid.getDurable().resolvePathname(pathname.substring(s + 1));
+    }
+
+    /**
+     * Perform lazy initialization.
+     *
+     * @throws Exception Any exceptions thrown during initialization.
+     */
+    private void _initialize()
+            throws Exception {
+        if (tuple != null)
+            return;
+        tupleFactories = getTupleFactories();
+        ReadableBytes readableBytes = null;
+        if (isSerialized()) {
+            readableBytes = readable();
+            _skipLen(readableBytes);
+        }
+        tuple = new PASerializable[_size()];
+        int i = 0;
+        _len = 0;
+        while (i < _size()) {
+            PASerializable elementJid = createSubordinate(tupleFactories[i], readableBytes);
+            _len += elementJid.getDurable().getSerializedLength();
+            tuple[i] = elementJid;
+            i += 1;
+        }
+    }
+
+    /**
+     * Skip over the length at the beginning of the serialized data.
+     *
+     * @param readableBytes Holds the serialized data.
+     */
+    private void _skipLen(ReadableBytes readableBytes) {
+        readableBytes.skip(Util.INT_LENGTH);
+    }
+
+    /**
+     * Returns the size of the serialized data (exclusive of its length header).
+     *
+     * @param readableBytes Holds the serialized data.
+     * @return The size of the serialized data (exclusive of its length header).
+     */
+    private int _loadLen(ReadableBytes readableBytes) {
+        return readableBytes.readInt();
+    }
+
+    /**
+     * Writes the size of the serialized data (exclusive of its length header).
+     *
+     * @param appendableBytes The object written to.
+     */
+    private void _saveLen(AppendableBytes appendableBytes) {
+        appendableBytes.writeInt(_len);
+    }
+
+    /**
+     * Returns the number of bytes needed to serialize the persistent data.
+     *
+     * @return The minimum size of the byte array needed to serialize the persistent data.
+     */
+    @Override
+    public int getSerializedLength()
+            throws Exception {
+        _initialize();
+        return Util.INT_LENGTH + _len;
+    }
+
+    /**
+     * Serialize the persistent data.
+     *
+     * @param appendableBytes The wrapped byte array into which the persistent data is to be serialized.
+     */
+    @Override
+    protected void serialize(AppendableBytes appendableBytes)
+            throws Exception {
+        _saveLen(appendableBytes);
+        int i = 0;
+        while (i < _size()) {
+            _iGet(i).getDurable().save(appendableBytes);
+            i += 1;
+        }
+    }
+
+    /**
+     * Load the serialized data into the JID.
+     *
+     * @param readableBytes Holds the serialized data.
+     */
+    @Override
+    public void load(ReadableBytes readableBytes)
+            throws Exception {
+        super.load(readableBytes);
+        _len = _loadLen(readableBytes);
+        tuple = null;
+        readableBytes.skip(_len);
+    }
+
+    /**
+     * Process a change in the persistent data.
+     *
+     * @param lengthChange The change in the size of the serialized data.
+     * @throws Exception Any uncaught exception which occurred while processing the change.
+     */
+    @Override
+    public void change(int lengthChange)
+            throws Exception {
+        _len += lengthChange;
+        super.change(lengthChange);
+    }
+}
