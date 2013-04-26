bcc71fc719fa2d8b1fcea1f37e081e231ca8c8a5
Bill La Forge <laforge49@gmail.com>
2013-04-26 17:54:27 +0530

    Updated DurableFactories

--- pactor-durable/src/main/java/org/agilewiki/pactor/durable/DurableFactories.java
+++ pactor-durable/src/main/java/org/agilewiki/pactor/durable/DurableFactories.java
@@ -3,6 +3,7 @@ package org.agilewiki.pactor.durable;
 import org.agilewiki.pactor.MailboxFactory;
 import org.agilewiki.pactor.durable.impl.IncDesFactory;
 import org.agilewiki.pactor.durable.impl.scalar.flens.*;
+import org.agilewiki.pactor.durable.impl.scalar.vlens.*;
 
 public class DurableFactories {
     public static MailboxFactory createMailboxFactory() throws Exception {
@@ -24,5 +25,17 @@ public class DurableFactories {
         PALongImpl.registerFactory(_factoryLocator);
         PAFloatImpl.registerFactory(_factoryLocator);
         PADoubleImpl.registerFactory(_factoryLocator);
+
+        BoxImpl.registerFactory(_factoryLocator);
+        RootImpl.registerFactory(_factoryLocator);
+        PAStringImpl.registerFactory(_factoryLocator);
+        BytesImpl.registerFactory(_factoryLocator);
+    }
+
+    public static void registerUnionFactory(final FactoryLocator _factoryLocator,
+                                            final String _subActorType,
+                                            final String... _actorTypes)
+            throws Exception {
+        UnionImpl.registerFactory(_factoryLocator, _subActorType, _actorTypes);
     }
 }
