e24c383c87c5cd6ffd2fde50d586729f669863f1
Bill La Forge <laforge49@gmail.com>
2013-04-30 16:58:47 +0530

    Reworked list and map factory registration

--- pactor-util/src/main/java/org/agilewiki/pactor/util/durable/Durables.java
+++ pactor-util/src/main/java/org/agilewiki/pactor/util/durable/Durables.java
@@ -5,6 +5,7 @@ import org.agilewiki.pactor.api.MailboxFactory;
 import org.agilewiki.pactor.impl.DefaultMailboxFactoryImpl;
 import org.agilewiki.pactor.util.Ancestor;
 import org.agilewiki.pactor.util.PAProperties;
+import org.agilewiki.pactor.util.durable.incDes.*;
 import org.agilewiki.pactor.utilImpl.durable.FactoryLocatorImpl;
 import org.agilewiki.pactor.utilImpl.durable.incDes.IncDesFactory;
 import org.agilewiki.pactor.utilImpl.durable.incDes.collection.blist.BListFactory;
@@ -62,11 +63,68 @@ public class Durables {
         PAStringImpl.registerFactory(_factoryLocator);
         BytesImpl.registerFactory(_factoryLocator);
 
-        BListFactory.registerFactories(_factoryLocator);
+        registerListFactory(_factoryLocator, PAList.PASTRING_LIST, PAString.FACTORY_NAME);
+        registerListFactory(_factoryLocator, PAList.BYTES_LIST, Bytes.FACTORY_NAME);
+        registerListFactory(_factoryLocator, PAList.BOX_LIST, Box.FACTORY_NAME);
+        registerListFactory(_factoryLocator, PAList.PALONG_LIST, PALong.FACTORY_NAME);
+        registerListFactory(_factoryLocator, PAList.PAINTEGER_LIST, PAInteger.FACTORY_NAME);
+        registerListFactory(_factoryLocator, PAList.PAFLOAT_LIST, PAFloat.FACTORY_NAME);
+        registerListFactory(_factoryLocator, PAList.PADOUBLE_LIST, PADouble.FACTORY_NAME);
+        registerListFactory(_factoryLocator, PAList.PABOOLEAN_LIST, PABoolean.FACTORY_NAME);
+
+        registerStringMapFactory(_factoryLocator, PAMap.STRING_PASTRING_MAP, PAString.FACTORY_NAME);
+        registerStringMapFactory(_factoryLocator, PAMap.STRING_BYTES_MAP, Bytes.FACTORY_NAME);
+        registerStringMapFactory(_factoryLocator, PAMap.STRING_BOX_MAP, Box.FACTORY_NAME);
+        registerStringMapFactory(_factoryLocator, PAMap.STRING_PALONG_MAP, PALong.FACTORY_NAME);
+        registerStringMapFactory(_factoryLocator, PAMap.STRING_PAINTEGER_MAP, PAInteger.FACTORY_NAME);
+        registerStringMapFactory(_factoryLocator, PAMap.STRING_PAFLOAT_MAP, PAFloat.FACTORY_NAME);
+        registerStringMapFactory(_factoryLocator, PAMap.STRING_PADOUBLE_MAP, PADouble.FACTORY_NAME);
+        registerStringMapFactory(_factoryLocator, PAMap.STRING_PABOOLEAN_MAP, PABoolean.FACTORY_NAME);
+
+
+        registerIntegerMapFactory(_factoryLocator, PAMap.INTEGER_PASTRING_MAP, PAString.FACTORY_NAME);
+        registerIntegerMapFactory(_factoryLocator, PAMap.INTEGER_BYTES_MAP, Bytes.FACTORY_NAME);
+        registerIntegerMapFactory(_factoryLocator, PAMap.INTEGER_BOX_MAP, Box.FACTORY_NAME);
+        registerIntegerMapFactory(_factoryLocator, PAMap.INTEGER_PALONG_MAP, PALong.FACTORY_NAME);
+        registerIntegerMapFactory(_factoryLocator, PAMap.INTEGER_PAINTEGER_MAP, PAInteger.FACTORY_NAME);
+        registerIntegerMapFactory(_factoryLocator, PAMap.INTEGER_PAFLOAT_MAP, PAFloat.FACTORY_NAME);
+        registerIntegerMapFactory(_factoryLocator, PAMap.INTEGER_PADOUBLE_MAP, PADouble.FACTORY_NAME);
+        registerIntegerMapFactory(_factoryLocator, PAMap.INTEGER_PABOOLEAN_MAP, PABoolean.FACTORY_NAME);
+
+        registerLongMapFactory(_factoryLocator, PAMap.LONG_PASTRING_MAP, PAString.FACTORY_NAME);
+        registerLongMapFactory(_factoryLocator, PAMap.LONG_BYTES_MAP, Bytes.FACTORY_NAME);
+        registerLongMapFactory(_factoryLocator, PAMap.LONG_BOX_MAP, Box.FACTORY_NAME);
+        registerLongMapFactory(_factoryLocator, PAMap.LONG_PALONG_MAP, PALong.FACTORY_NAME);
+        registerLongMapFactory(_factoryLocator, PAMap.LONG_PAINTEGER_MAP, PAInteger.FACTORY_NAME);
+        registerLongMapFactory(_factoryLocator, PAMap.LONG_PAFLOAT_MAP, PAFloat.FACTORY_NAME);
+        registerLongMapFactory(_factoryLocator, PAMap.LONG_PADOUBLE_MAP, PADouble.FACTORY_NAME);
+        registerLongMapFactory(_factoryLocator, PAMap.LONG_PABOOLEAN_MAP, PABoolean.FACTORY_NAME);
 
         StringBMapFactory.registerFactories(_factoryLocator);
-        LongBMapFactory.registerFactories(_factoryLocator);
-        IntegerBMapFactory.registerFactories(_factoryLocator);
+    }
+
+    public static void registerListFactory(final FactoryLocator _factoryLocator,
+                                           final String _actorType,
+                                           final String _valueType) {
+        BListFactory.registerFactory(_factoryLocator, _actorType, _valueType);
+    }
+
+    public static void registerStringMapFactory(final FactoryLocator _factoryLocator,
+                                                 final String _actorType,
+                                                 final String _valueType) {
+        StringBMapFactory.registerFactory(_factoryLocator, _actorType, _valueType);
+    }
+
+    public static void registerIntegerMapFactory(final FactoryLocator _factoryLocator,
+                                                 final String _actorType,
+                                                 final String _valueType) {
+        IntegerBMapFactory.registerFactory(_factoryLocator, _actorType, _valueType);
+    }
+
+    public static void registerLongMapFactory(final FactoryLocator _factoryLocator,
+                                                 final String _actorType,
+                                                 final String _valueType) {
+        LongBMapFactory.registerFactory(_factoryLocator, _actorType, _valueType);
     }
 
     public static void registerUnionFactory(final FactoryLocator _factoryLocator,
--- pactor-util/src/main/java/org/agilewiki/pactor/utilImpl/durable/incDes/collection/bmap/IntegerBMapFactory.java
+++ pactor-util/src/main/java/org/agilewiki/pactor/utilImpl/durable/incDes/collection/bmap/IntegerBMapFactory.java
@@ -16,17 +16,6 @@ public class IntegerBMapFactory extends FactoryImpl {
 
     private final static int NODE_CAPACITY = 28;
 
-    public static void registerFactories(final FactoryLocator _factoryLocator) {
-        registerFactory(_factoryLocator, PAMap.INTEGER_PASTRING_MAP, PAString.FACTORY_NAME);
-        registerFactory(_factoryLocator, PAMap.INTEGER_BYTES_MAP, Bytes.FACTORY_NAME);
-        registerFactory(_factoryLocator, PAMap.INTEGER_BOX_MAP, Box.FACTORY_NAME);
-        registerFactory(_factoryLocator, PAMap.INTEGER_PALONG_MAP, PALong.FACTORY_NAME);
-        registerFactory(_factoryLocator, PAMap.INTEGER_PAINTEGER_MAP, PAInteger.FACTORY_NAME);
-        registerFactory(_factoryLocator, PAMap.INTEGER_PAFLOAT_MAP, PAFloat.FACTORY_NAME);
-        registerFactory(_factoryLocator, PAMap.INTEGER_PADOUBLE_MAP, PADouble.FACTORY_NAME);
-        registerFactory(_factoryLocator, PAMap.INTEGER_PABOOLEAN_MAP, PABoolean.FACTORY_NAME);
-    }
-
     public static void registerFactory(FactoryLocator factoryLocator,
                                        String actorType,
                                        String valueType) {
