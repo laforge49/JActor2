4840571041a241a9c9dc55381e70c30bb1bb7343
Bill La Forge <laforge49@gmail.com>
2013-05-02 18:28:18 +0530

    javadoc for JASerializable

--- jactor-util/src/main/java/org/agilewiki/jactor/util/durable/JASerializable.java
+++ jactor-util/src/main/java/org/agilewiki/jactor/util/durable/JASerializable.java
@@ -2,6 +2,15 @@ package org.agilewiki.jactor.util.durable;
 
 import org.agilewiki.jactor.util.durable.incDes.IncDes;
 
+/**
+ * All incrementally deserializable objects must implement this interface
+ * and return a reference to their durable part.
+ */
 public interface JASerializable {
+    /**
+     * Returns a non-null reference to the objects durable part.
+     *
+     * @return The durable part of a serializable object.
+     */
     IncDes getDurable();
 }
