fcb6aafab7717b15ef631241762d0caba2842f8e
Bill La Forge <laforge49@gmail.com>
2013-05-04 17:43:49 +0530

    javadoc for Root

--- jactor-util/src/main/java/org/agilewiki/jactor/util/durable/incDes/Root.java
+++ jactor-util/src/main/java/org/agilewiki/jactor/util/durable/incDes/Root.java
@@ -1,9 +1,21 @@
 package org.agilewiki.jactor.util.durable.incDes;
 
+/**
+ * The root node in a tree of serializable objects.
+ */
 public interface Root extends Box {
 
+    /**
+     * The factory name of a Root object.
+     */
     public static final String FACTORY_NAME = "root";
 
+    /**
+     * Returns the descriptor of the bundle whose factoryLocator can instantiate the objects
+     * in this tree.
+     *
+     * @return A descriptor in the form bundleName|version|location.
+     */
     String getDescriptor()
             throws Exception;
 }
