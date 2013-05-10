98f1c05cf86e9b5a53f8d779d984e04d240b23db
Bill La Forge <laforge49@gmail.com>
2013-05-10 15:37:49 +0530

    javadoc for FactoryLocatorActivator0

--- jactor-util/src/main/java/org/agilewiki/jactor/util/osgi/durable/FactoryLocatorActivator0.java
+++ jactor-util/src/main/java/org/agilewiki/jactor/util/osgi/durable/FactoryLocatorActivator0.java
@@ -9,7 +9,14 @@ import org.osgi.framework.BundleContext;
 
 import java.util.Hashtable;
 
+/**
+ * A basic activator with a registered factory locator service but no mailbox.
+ */
 public class FactoryLocatorActivator0 extends MailboxFactoryActivator {
+
+    /**
+     * The factory locator service.
+     */
     protected OsgiFactoryLocator factoryLocator;
 
     @Override
@@ -23,15 +30,26 @@ public class FactoryLocatorActivator0 extends MailboxFactoryActivator {
         setClosing();
     }
 
+    /**
+     * Returns the factory locator of the bundle.
+     *
+     * @return The factory locator.
+     */
     protected final FactoryLocator getFactoryLocator() {
         return factoryLocator;
     }
 
+    /**
+     * Create, initialize and register the factory locator.
+     */
     protected final void factoryLocatorStart() throws Exception {
         createFactoryLocator();
         factoryLocator.register(bundleContext);
     }
 
+    /**
+     * Create and initialize the factory locator.
+     */
     protected void createFactoryLocator() throws Exception {
         factoryLocator = new OsgiFactoryLocator();
     }
