abf0b15ac81f22985662ad7e27d90818129c91a8
Bill La Forge <laforge49@gmail.com>
2013-05-10 15:47:30 +0530

    (minimal) javadoc for DurableActivator

--- jactor-util/src/main/java/org/agilewiki/jactor/util/osgi/durable/DurablesActivator.java
+++ jactor-util/src/main/java/org/agilewiki/jactor/util/osgi/durable/DurablesActivator.java
@@ -2,14 +2,14 @@ package org.agilewiki.jactor.util.osgi.durable;
 
 import org.agilewiki.jactor.util.durable.Durables;
 
+/**
+ * Activator for the jactor-util bundle.
+ */
 public class DurablesActivator extends FactoryLocatorActivator0 {
+
     @Override
     protected void createFactoryLocator() throws Exception {
         super.createFactoryLocator();
         Durables.registerFactories(getFactoryLocator());
-        System.out.println("&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*");
-        System.out.println("&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*");
-        System.out.println("&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*");
-        System.out.println("&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*");
     }
 }
