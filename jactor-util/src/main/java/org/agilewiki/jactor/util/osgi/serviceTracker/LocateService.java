e1f68986f8f7f387ab973a8b657d5c2797cdf830
Bill La Forge <laforge49@gmail.com>
2013-05-19 11:26:36 +0530

    LocateService is a simplified service tracker

---
+++ jactor-util/src/main/java/org/agilewiki/jactor/util/osgi/serviceTracker/LocateService.java
@@ -0,0 +1,53 @@
+package org.agilewiki.jactor.util.osgi.serviceTracker;
+
+import org.agilewiki.jactor.api.Mailbox;
+import org.agilewiki.jactor.api.Request;
+import org.agilewiki.jactor.api.RequestBase;
+import org.agilewiki.jactor.api.Transport;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceReference;
+
+import java.util.Map;
+
+/**
+ * A simplified service tracker.
+ */
+public class LocateService<T> implements ServiceChangeReceiver<T> {
+    private Mailbox mailbox;
+    JAServiceTracker<T> tracker;
+    Transport<T> transport;
+
+    public LocateService(String clazz) throws Exception {
+        tracker = new JAServiceTracker(mailbox, clazz);
+    }
+
+    public Request<T> getReq() {
+        return new RequestBase<T>(mailbox) {
+            @Override
+            public void processRequest(final Transport<T> _transport) throws Exception {
+                tracker.startReq(LocateService.this).signal(mailbox);
+                transport = _transport;
+            }
+        };
+    }
+
+    @Override
+    public void serviceChange(ServiceEvent _event,
+                              Map<ServiceReference, T> _tracked,
+                              Transport _transport)
+            throws Exception {
+        _transport.processResponse(null);
+        if (_tracked.size() > 0 && transport != null) {
+            T service = _tracked.values().iterator().next();
+            transport.processResponse(service);
+            transport = null;
+            tracker.close();
+            tracker = null;
+        }
+    }
+
+    @Override
+    public Mailbox getMailbox() {
+        return mailbox;
+    }
+}
