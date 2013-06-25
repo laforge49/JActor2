55548ba5678f948a61e0230720ae01b68c8aa087
Bill La Forge <laforge49@gmail.com>
2013-06-25 09:37:54 +0530

    javadocs for UtilMailboxFactory

--- jactor-util/src/main/java/org/agilewiki/jactor/util/UtilMailboxFactory.java
+++ jactor-util/src/main/java/org/agilewiki/jactor/util/UtilMailboxFactory.java
@@ -5,11 +5,25 @@ import org.agilewiki.jactor.impl.JAMailbox;
 import org.agilewiki.jactor.impl.MessageQueue;
 import org.slf4j.Logger;
 
-public final class UtilMailboxFactory<M extends JAMailbox> extends DefaultMailboxFactoryImpl<M> {
+/**
+ * A Mailbox factory
+ * @param <M>    The type of mailbox.
+ */
+public final class UtilMailboxFactory<M extends JAMailbox>
+        extends DefaultMailboxFactoryImpl<M> {
+
+    /**
+     * Create a mailbox factory with the default thread pool size for mailboxes that may block.
+     */
     public UtilMailboxFactory() {
         super();
     }
 
+    /**
+     * Create a mailbox factory.
+     *
+     * @param maxBlockingThreads    The thread pool size for mailboxes that may block.
+     */
     public UtilMailboxFactory(final int maxBlockingThreads) {
         super(maxBlockingThreads);
     }
