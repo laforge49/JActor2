7c2e752ea7ba9a285ddde3137ef269e425a52d6b
Bill La Forge <laforge49@gmail.com>
2013-07-13 09:45:34 +0530

    Updated ThreadManager javadoc

--- jactor2-impl/src/main/java/org/agilewiki/jactor2/impl/ThreadManager.java
+++ jactor2-impl/src/main/java/org/agilewiki/jactor2/impl/ThreadManager.java
@@ -6,22 +6,24 @@ import java.util.concurrent.ThreadFactory;
  * A ThreadManager is used to process a collection of Runnable tasks.
  * ThreadManager is a thread pool, but with a simplified API and
  * assumes that the thread pool has a fixed number of threads.
+ * ThreadManager is also responsible for setting the threadReference
+ * in the mailbox.
  */
 public interface ThreadManager extends AutoCloseable {
     /**
-     * Create and start the concurrent.
+     * Create and start the threads.
      *
-     * @param threadCount   The number of concurrent to be used.
-     * @param threadFactory Used to create the concurrent.
+     * @param _threadCount   The number of threads to be created.
+     * @param _threadFactory Used to create the threads.
      */
-    public void start(final int threadCount, final ThreadFactory threadFactory);
+    public void start(final int _threadCount, final ThreadFactory _threadFactory);
 
     /**
      * Begin running a mailbox.
      *
-     * @param mailbox The run method is to be called by another thread.
+     * @param _mailbox The run method is to be called by the selected thread.
      */
-    public void execute(final UnboundMailbox mailbox);
+    public void execute(final UnboundMailbox _mailbox);
 
     /**
      * Stop all the threads as they complete their tasks.
