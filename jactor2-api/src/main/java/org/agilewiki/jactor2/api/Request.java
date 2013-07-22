a289f199c18e1293c3edf05bdd868e4076484789
Bill La Forge <laforge49@gmail.com>
2013-07-22 12:13:23 +0530

    Request now has nested classes

--- jactor2-api/src/main/java/org/agilewiki/jactor2/api/Request.java
+++ jactor2-api/src/main/java/org/agilewiki/jactor2/api/Request.java
@@ -106,8 +106,8 @@ public abstract class Request<RESPONSE_TYPE> {
      * @throws Exception If the result is an exception, it is thrown rather than being returned.
      */
     public RESPONSE_TYPE call() throws Exception {
-        final Caller caller = new Caller();
-        final RequestMessage message = new RequestMessage(true, caller, null,
+        final Pender pender = new Pender();
+        final RequestMessage message = new RequestMessage(true, pender, null,
                 this, null, DummyResponseProcessor.SINGLETON);
         return (RESPONSE_TYPE) message.call(mailbox);
     }
@@ -122,228 +122,228 @@ public abstract class Request<RESPONSE_TYPE> {
      */
     abstract public void processRequest(final Transport<RESPONSE_TYPE> _transport)
             throws Exception;
-}
 
-class Caller implements MessageSource {
+    private static class Pender implements MessageSource {
 
-    /**
-     * Used to signal the arrival of a response.
-     */
-    private final Semaphore done = new Semaphore(0);
+        /**
+         * Used to signal the arrival of a response.
+         */
+        private final Semaphore done = new Semaphore(0);
 
-    /**
-     * The result from the incoming response. May be null or an Exception.
-     */
-    private transient Object result;
+        /**
+         * The result from the incoming response. May be null or an Exception.
+         */
+        private transient Object result;
 
-    /**
-     * Returns the response, which may be null. But if the response
-     * is an exception, then it is thrown.
-     *
-     * @return The response or null, but not an exception.
-     */
-    public Object call() throws Exception {
-        done.acquire();
-        if (result instanceof Exception)
-            throw (Exception) result;
-        if (result instanceof Error)
-            throw (Error) result;
-        return result;
-    }
+        /**
+         * Returns the response, which may be null. But if the response
+         * is an exception, then it is thrown.
+         *
+         * @return The response or null, but not an exception.
+         */
+        Object pend() throws Exception {
+            done.acquire();
+            if (result instanceof Exception)
+                throw (Exception) result;
+            if (result instanceof Error)
+                throw (Error) result;
+            return result;
+        }
 
-    @Override
-    public void incomingResponse(final Message _message,
-                                 final Mailbox _responseSource) {
-        this.result = ((RequestMessage) _message).getResponse();
-        done.release();
-    }
-}
-
-class RequestMessage implements Message {
-    protected final boolean foreign;
-    protected final MessageSource messageSource;
-    protected final Message oldMessage;
-    protected final Request<?> request;
-    protected final ExceptionHandler sourceExceptionHandler;
-    protected final ResponseProcessor<?> responseProcessor;
-    protected boolean responsePending = true;
-    protected Object response;
-
-    public <E, A extends Actor> RequestMessage(final boolean _foreign,
-                                               final MessageSource _source,
-                                               final Message _old,
-                                               final Request<?> _request,
-                                               final ExceptionHandler _handler,
-                                               final ResponseProcessor _rp) {
-        messageSource = _source;
-        foreign = _foreign;
-        oldMessage = _old;
-        request = _request;
-        sourceExceptionHandler = _handler;
-        responseProcessor = _rp;
-    }
-
-    @Override
-    public boolean isForeign() {
-        return foreign;
-    }
-
-    @Override
-    public boolean isResponsePending() {
-        return responsePending;
-    }
-
-    /**
-     * @param _response the response being returned
-     */
-    public void setResponse(final Object _response) {
-        responsePending = false;
-        response = _response;
-    }
-
-    /**
-     * @return the response
-     */
-    public Object getResponse() {
-        return response;
-    }
-
-    /**
-     * @return the responseProcessor
-     */
-    public ResponseProcessor<?> getResponseProcessor() {
-        return responseProcessor;
-    }
-
-    public final void signal(final Mailbox _targetMailbox) throws Exception {
-        Mailbox sourceMailbox = (Mailbox) messageSource;
-        boolean local = sourceMailbox == _targetMailbox;
-        if (local || !sourceMailbox.buffer(this, _targetMailbox))
-            _targetMailbox.unbufferedAddMessages(this, local);
-    }
-
-    public final void send(final Mailbox _targetMailbox) throws Exception {
-        Mailbox sourceMailbox = (Mailbox) messageSource;
-        boolean local = sourceMailbox == _targetMailbox;
-        if (local || !sourceMailbox.buffer(this, _targetMailbox))
-            _targetMailbox.unbufferedAddMessages(this, local);
-    }
-
-    public final Object call(final Mailbox _targetMailbox) throws Exception {
-        _targetMailbox.unbufferedAddMessages(this, false);
-        return ((Caller) messageSource).call();
-    }
-
-    @Override
-    public void close() throws Exception {
-        if (!responsePending)
-            return;
-        responsePending = false;
-        response = new ServiceClosedException();
-        messageSource.incomingResponse(this, null);
-    }
-
-    public void eval(final Mailbox _targetMailbox) {
-        if (responsePending) {
-            processRequestMessage(_targetMailbox);
-        } else {
-            processResponseMessage(_targetMailbox);
+        @Override
+        public void incomingResponse(final Message _message,
+                                     final Mailbox _responseSource) {
+            this.result = ((RequestMessage) _message).getResponse();
+            done.release();
         }
     }
 
-    private void processRequestMessage(final Mailbox _targetMailbox) {
-        final MailboxFactory mailboxFactory = _targetMailbox.getMailboxFactory();
-        if (foreign)
-            mailboxFactory.addAutoClosable(this);
-        _targetMailbox.setExceptionHandler(null);
-        _targetMailbox.setCurrentMessage(this);
-        try {
-            request.processRequest(
-                    new Transport() {
-                        @Override
-                        public void processResponse(final Object response)
-                                throws Exception {
-                            if (foreign)
-                                mailboxFactory.removeAutoClosable(RequestMessage.this);
-                            if (!responsePending)
-                                return;
-                            if (getResponseProcessor() != EventResponseProcessor.SINGLETON) {
-                                setResponse(response);
-                                messageSource.incomingResponse(RequestMessage.this, _targetMailbox);
-                            } else {
-                                if (response instanceof Throwable) {
-                                    _targetMailbox.getLogger().warn("Uncaught throwable",
-                                            (Throwable) response);
+    private static class RequestMessage implements Message {
+        protected final boolean foreign;
+        protected final MessageSource messageSource;
+        protected final Message oldMessage;
+        protected final Request<?> request;
+        protected final ExceptionHandler sourceExceptionHandler;
+        protected final ResponseProcessor<?> responseProcessor;
+        protected boolean responsePending = true;
+        protected Object response;
+
+        <E, A extends Actor> RequestMessage(final boolean _foreign,
+                                            final MessageSource _source,
+                                            final Message _old,
+                                            final Request<?> _request,
+                                            final ExceptionHandler _handler,
+                                            final ResponseProcessor _rp) {
+            messageSource = _source;
+            foreign = _foreign;
+            oldMessage = _old;
+            request = _request;
+            sourceExceptionHandler = _handler;
+            responseProcessor = _rp;
+        }
+
+        @Override
+        public boolean isForeign() {
+            return foreign;
+        }
+
+        @Override
+        public boolean isResponsePending() {
+            return responsePending;
+        }
+
+        /**
+         * @param _response the response being returned
+         */
+        void setResponse(final Object _response) {
+            responsePending = false;
+            response = _response;
+        }
+
+        /**
+         * @return the response
+         */
+        Object getResponse() {
+            return response;
+        }
+
+        /**
+         * @return the responseProcessor
+         */
+        ResponseProcessor<?> getResponseProcessor() {
+            return responseProcessor;
+        }
+
+        final void signal(final Mailbox _targetMailbox) throws Exception {
+            Mailbox sourceMailbox = (Mailbox) messageSource;
+            boolean local = sourceMailbox == _targetMailbox;
+            if (local || !sourceMailbox.buffer(this, _targetMailbox))
+                _targetMailbox.unbufferedAddMessages(this, local);
+        }
+
+        final void send(final Mailbox _targetMailbox) throws Exception {
+            Mailbox sourceMailbox = (Mailbox) messageSource;
+            boolean local = sourceMailbox == _targetMailbox;
+            if (local || !sourceMailbox.buffer(this, _targetMailbox))
+                _targetMailbox.unbufferedAddMessages(this, local);
+        }
+
+        final Object call(final Mailbox _targetMailbox) throws Exception {
+            _targetMailbox.unbufferedAddMessages(this, false);
+            return ((Pender) messageSource).pend();
+        }
+
+        @Override
+        public void close() throws Exception {
+            if (!responsePending)
+                return;
+            responsePending = false;
+            response = new ServiceClosedException();
+            messageSource.incomingResponse(this, null);
+        }
+
+        public void eval(final Mailbox _targetMailbox) {
+            if (responsePending) {
+                processRequestMessage(_targetMailbox);
+            } else {
+                processResponseMessage(_targetMailbox);
+            }
+        }
+
+        private void processRequestMessage(final Mailbox _targetMailbox) {
+            final MailboxFactory mailboxFactory = _targetMailbox.getMailboxFactory();
+            if (foreign)
+                mailboxFactory.addAutoClosable(this);
+            _targetMailbox.setExceptionHandler(null);
+            _targetMailbox.setCurrentMessage(this);
+            try {
+                request.processRequest(
+                        new Transport() {
+                            @Override
+                            public void processResponse(final Object response)
+                                    throws Exception {
+                                if (foreign)
+                                    mailboxFactory.removeAutoClosable(RequestMessage.this);
+                                if (!responsePending)
+                                    return;
+                                if (getResponseProcessor() != EventResponseProcessor.SINGLETON) {
+                                    setResponse(response);
+                                    messageSource.incomingResponse(RequestMessage.this, _targetMailbox);
+                                } else {
+                                    if (response instanceof Throwable) {
+                                        _targetMailbox.getLogger().warn("Uncaught throwable",
+                                                (Throwable) response);
+                                    }
                                 }
                             }
-                        }
 
-                        @Override
-                        public MailboxFactory getMailboxFactory() {
-                            if (messageSource == null)
-                                return null;
-                            if (!(messageSource instanceof Mailbox))
-                                return null;
-                            return ((Mailbox) messageSource).getMailboxFactory();
-                        }
+                            @Override
+                            public MailboxFactory getMailboxFactory() {
+                                if (messageSource == null)
+                                    return null;
+                                if (!(messageSource instanceof Mailbox))
+                                    return null;
+                                return ((Mailbox) messageSource).getMailboxFactory();
+                            }
 
-                        @Override
-                        public void processException(Exception response) throws Exception {
-                            processResponse((Object) response);
-                        }
-                    });
-        } catch (final Throwable t) {
-            if (foreign)
-                mailboxFactory.removeAutoClosable(this);
-            processThrowable(_targetMailbox, t);
-        }
-    }
-
-    private void processResponseMessage(final Mailbox _sourceMailbox) {
-        _sourceMailbox.setExceptionHandler(sourceExceptionHandler);
-        _sourceMailbox.setCurrentMessage(oldMessage);
-        if (response instanceof Throwable) {
-            oldMessage.processThrowable(_sourceMailbox, (Throwable) response);
-            return;
-        }
-        @SuppressWarnings("rawtypes")
-        final ResponseProcessor responseProcessor = this
-                .getResponseProcessor();
-        try {
-            responseProcessor.processResponse(response);
-        } catch (final Throwable t) {
-            oldMessage.processThrowable(_sourceMailbox, t);
-        }
-    }
-
-    @Override
-    public void processThrowable(final Mailbox _activeMailbox, final Throwable _t) {
-        ExceptionHandler exceptionHandler = _activeMailbox.getExceptionHandler();
-        if (exceptionHandler != null) {
-            try {
-                exceptionHandler.processException(_t);
-            } catch (final Throwable u) {
-                _activeMailbox.getLogger().error("Exception handler unable to process throwable "
-                        + exceptionHandler.getClass().getName(), u);
-                if (!(responseProcessor instanceof EventResponseProcessor)) {
-                    if (!responsePending)
-                        return;
-                    setResponse(u);
-                    messageSource.incomingResponse(this, _activeMailbox);
-                } else {
-                    _activeMailbox.getLogger().error("Thrown by exception handler and uncaught "
-                            + exceptionHandler.getClass().getName(), _t);
-                }
+                            @Override
+                            public void processException(Exception response) throws Exception {
+                                processResponse((Object) response);
+                            }
+                        });
+            } catch (final Throwable t) {
+                if (foreign)
+                    mailboxFactory.removeAutoClosable(this);
+                processThrowable(_targetMailbox, t);
             }
-        } else {
-            if (!responsePending) {
+        }
+
+        private void processResponseMessage(final Mailbox _sourceMailbox) {
+            _sourceMailbox.setExceptionHandler(sourceExceptionHandler);
+            _sourceMailbox.setCurrentMessage(oldMessage);
+            if (response instanceof Throwable) {
+                oldMessage.processThrowable(_sourceMailbox, (Throwable) response);
                 return;
             }
-            setResponse(_t);
-            if (!(responseProcessor instanceof EventResponseProcessor))
-                messageSource.incomingResponse(this, _activeMailbox);
-            else {
-                _activeMailbox.getLogger().warn("Uncaught throwable", _t);
+            @SuppressWarnings("rawtypes")
+            final ResponseProcessor responseProcessor = this
+                    .getResponseProcessor();
+            try {
+                responseProcessor.processResponse(response);
+            } catch (final Throwable t) {
+                oldMessage.processThrowable(_sourceMailbox, t);
+            }
+        }
+
+        @Override
+        public void processThrowable(final Mailbox _activeMailbox, final Throwable _t) {
+            ExceptionHandler exceptionHandler = _activeMailbox.getExceptionHandler();
+            if (exceptionHandler != null) {
+                try {
+                    exceptionHandler.processException(_t);
+                } catch (final Throwable u) {
+                    _activeMailbox.getLogger().error("Exception handler unable to process throwable "
+                            + exceptionHandler.getClass().getName(), u);
+                    if (!(responseProcessor instanceof EventResponseProcessor)) {
+                        if (!responsePending)
+                            return;
+                        setResponse(u);
+                        messageSource.incomingResponse(this, _activeMailbox);
+                    } else {
+                        _activeMailbox.getLogger().error("Thrown by exception handler and uncaught "
+                                + exceptionHandler.getClass().getName(), _t);
+                    }
+                }
+            } else {
+                if (!responsePending) {
+                    return;
+                }
+                setResponse(_t);
+                if (!(responseProcessor instanceof EventResponseProcessor))
+                    messageSource.incomingResponse(this, _activeMailbox);
+                else {
+                    _activeMailbox.getLogger().warn("Uncaught throwable", _t);
+                }
             }
         }
     }
