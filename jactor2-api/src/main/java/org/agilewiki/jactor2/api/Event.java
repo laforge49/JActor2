88c84f3000740cf3cf7cc285b64793f58e501477
Bill La Forge <laforge49@gmail.com>
2013-07-22 10:54:54 +0530

    EventMessage is now a nested class.

--- jactor2-api/src/main/java/org/agilewiki/jactor2/api/Event.java
+++ jactor2-api/src/main/java/org/agilewiki/jactor2/api/Event.java
@@ -30,7 +30,7 @@ public abstract class Event<TARGET_ACTOR_TYPE extends Actor> {
      * @param _targetActor The actor being operated on.
      */
     public void signal(final TARGET_ACTOR_TYPE _targetActor) throws Exception {
-        final EventMessage message = new EventMessage(_targetActor, this);
+        final EventMessage message = new EventMessage(_targetActor);
         message.event();
     }
 
@@ -42,56 +42,54 @@ public abstract class Event<TARGET_ACTOR_TYPE extends Actor> {
      */
     abstract public void processEvent(final TARGET_ACTOR_TYPE _targetActor)
             throws Exception;
-}
 
-class EventMessage extends Message {
-    protected final Actor targetActor;
-    protected final Event event;
+    private class EventMessage extends Message {
+        protected final TARGET_ACTOR_TYPE targetActor;
 
-    public <E, A extends Actor> EventMessage(final A _targetActor, final Event _event) {
-        targetActor = _targetActor;
-        event = _event;
-    }
-
-    @Override
-    public boolean isForeign() {
-        return false;
-    }
-
-    @Override
-    public boolean isResponsePending() {
-        return false;
-    }
-
-    public final void event()
-            throws Exception {
-        targetActor.getMailbox().unbufferedAddMessages(this, false);
-    }
-
-    @Override
-    public void close() throws Exception {
-    }
-
-    public void eval(final Mailbox _targetMailbox) {
-        _targetMailbox.setExceptionHandler(null);
-        _targetMailbox.setCurrentMessage(this);
-        try {
-            event.processEvent(targetActor);
-        } catch (final Throwable t) {
-            processThrowable(_targetMailbox, t);
+        public EventMessage(final TARGET_ACTOR_TYPE _targetActor) {
+            targetActor = _targetActor;
         }
-    }
 
-    protected void processThrowable(final Mailbox _activeMailbox, final Throwable _t) {
-        ExceptionHandler exceptionHandler = _activeMailbox.getExceptionHandler();
-        if (exceptionHandler != null) {
+        @Override
+        public boolean isForeign() {
+            return false;
+        }
+
+        @Override
+        public boolean isResponsePending() {
+            return false;
+        }
+
+        public final void event()
+                throws Exception {
+            targetActor.getMailbox().unbufferedAddMessages(this, false);
+        }
+
+        @Override
+        public void close() throws Exception {
+        }
+
+        public void eval(final Mailbox _targetMailbox) {
+            _targetMailbox.setExceptionHandler(null);
+            _targetMailbox.setCurrentMessage(this);
             try {
-                exceptionHandler.processException(_t);
-            } catch (final Throwable u) {
-                _activeMailbox.getLogger().error("Exception handler unable to process throwable "
-                        + exceptionHandler.getClass().getName(), u);
-                _activeMailbox.getLogger().error("Thrown by exception handler and uncaught "
-                        + exceptionHandler.getClass().getName(), _t);
+                processEvent(targetActor);
+            } catch (final Throwable t) {
+                processThrowable(_targetMailbox, t);
+            }
+        }
+
+        protected void processThrowable(final Mailbox _activeMailbox, final Throwable _t) {
+            ExceptionHandler exceptionHandler = _activeMailbox.getExceptionHandler();
+            if (exceptionHandler != null) {
+                try {
+                    exceptionHandler.processException(_t);
+                } catch (final Throwable u) {
+                    _activeMailbox.getLogger().error("Exception handler unable to process throwable "
+                            + exceptionHandler.getClass().getName(), u);
+                    _activeMailbox.getLogger().error("Thrown by exception handler and uncaught "
+                            + exceptionHandler.getClass().getName(), _t);
+                }
             }
         }
     }
