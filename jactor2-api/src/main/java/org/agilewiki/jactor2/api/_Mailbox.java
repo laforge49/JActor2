85593c69e1ccb323ff5dd9a71d9f7c980dae13b3
Bill La Forge <laforge49@gmail.com>
2013-07-23 15:53:46 +0530

    updated javadocs for _Mailbox

--- jactor2-api/src/main/java/org/agilewiki/jactor2/api/_Mailbox.java
+++ jactor2-api/src/main/java/org/agilewiki/jactor2/api/_Mailbox.java
@@ -3,13 +3,12 @@ package org.agilewiki.jactor2.api;
 import org.slf4j.Logger;
 
 /**
- * _Mailbox defines the internal API used by Request and Event
- * to pass _Request's to a target mailbox.
+ * _Mailbox defines an internal API of Mailbox used by Request and Event.
  */
 interface _Mailbox extends MessageSource {
 
     /**
-     * Add a message directly to the queue.
+     * Add a message directly to the input queue of a Mailbox.
      *
      * @param message A message.
      * @param local   True when the current thread is bound to the mailbox.
@@ -18,11 +17,11 @@ interface _Mailbox extends MessageSource {
             throws Exception;
 
     /**
-     * Returns true, if the message was buffered for sending later.
+     * Buffers a message in the sending mailbox for sending later.
      *
-     * @param message Message to send-buffer
+     * @param message Message to be buffered.
      * @param target  The mailbox that should eventually receive this message
-     * @return true, if buffered
+     * @return True if the message was buffered.
      */
     boolean buffer(final Message message, final Mailbox target);
 
@@ -31,11 +30,28 @@ interface _Mailbox extends MessageSource {
      */
     boolean isRunning();
 
+    /**
+     * Returns the message currently being processed.
+     * @return The message currently being processed.
+     */
     Message getCurrentMessage();
 
+    /**
+     * The current exception handler.
+     *
+     * @return The current exception handler, or null.
+     */
     ExceptionHandler getExceptionHandler();
 
+    /**
+     * Returns the mailbox logger.
+     * @return The mailbox logger.
+     */
     Logger getLogger();
 
+    /**
+     * Identify the message currently being processed.
+     * @param message The message currently being processed.
+     */
     void setCurrentMessage(Message message);
 }
