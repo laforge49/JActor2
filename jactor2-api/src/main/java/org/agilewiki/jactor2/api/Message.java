74fb45dfeccc426aa8c25fb23578157d4ad046fc
Bill La Forge <laforge49@gmail.com>
2013-07-24 09:39:53 +0530

    updated javadocs for Message

--- jactor2-api/src/main/java/org/agilewiki/jactor2/api/Message.java
+++ jactor2-api/src/main/java/org/agilewiki/jactor2/api/Message.java
@@ -2,30 +2,40 @@ package org.agilewiki.jactor2.api;
 
 /**
  * <p>
- * Message encapsulates the user/application Request which are queued in the Actor's mailbox. The lightweight
- * thread associated with the Actor's mailbox will process the Message asynchronously. Considering the
- * scenario where multiple Actors are required to do the processing e.g PActor1 ---> PActor2 ---> PActor3.
- * The initial Request is passed to the PActor1 which might further pass the ResponseProcessor to PActor2
- * and PActor2 further to PActor3. The send method of the MailBox is used to pass ResponseProcessor across
- * PActor chain as considered in the scenario.
+ * Message encapsulates the user/application Request/Event which are queued in the Actor's mailbox. The lightweight
+ * thread associated with the Actor's mailbox will process the Message asynchronously.
  * </p>
  */
 
 abstract public interface Message extends AutoCloseable {
 
     /**
-     * Returns true when the response is to be sent to another mailbox factory.
+     * Returns true when the response is to be sent to a mailbox created from a different mailbox factory.
      *
-     * @return True when the response is to be sent to another mailbox factory.
+     * @return True when the response is to be sent to a mailbox created from a different mailbox factory.
      */
     boolean isForeign();
 
     /**
-     * @return the responsePending
+     * Returns true when a response is expected but has not yet been placed in the message.
+     *
+     * @return True when a response is expected but has not yet been placed in the message.
      */
     boolean isResponsePending();
 
+    /**
+     * Execute the Event.processEvent or Request.processRequest method
+     * of the event/request held by the message.
+     *
+     * @param _targetMailbox The mailbox whose thread is to evaluate the event/request.
+     */
     void eval(final Mailbox _targetMailbox);
 
+    /**
+     * Process the throwable on the current thread in the context of the active mailbox.
+     *
+     * @param _activeMailbox The mailbox providing the context for processing the throwable.
+     * @param _t             The throwable to be processed.
+     */
     void processThrowable(final Mailbox _activeMailbox, final Throwable _t);
 }
