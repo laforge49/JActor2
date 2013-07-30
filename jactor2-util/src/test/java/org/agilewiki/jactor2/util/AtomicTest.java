66ab1551c0653888a50baaf37180bf49f3ef929b
Bill La Forge <laforge49@gmail.com>
2013-07-30 20:08:14 +0530

    Dropped atomic package, converted AtomicTest.

--- jactor2-util/src/test/java/org/agilewiki/jactor2/util/AtomicTest.java
+++ jactor2-util/src/test/java/org/agilewiki/jactor2/util/AtomicTest.java
@@ -1,4 +1,4 @@
-package org.agilewiki.jactor2.util.atomic;
+package org.agilewiki.jactor2.util;
 
 import junit.framework.TestCase;
 import org.agilewiki.jactor2.api.Mailbox;
@@ -12,11 +12,10 @@ import org.agilewiki.jactor2.util.UtilMailboxFactory;
 public class AtomicTest extends TestCase {
     int count = 0;
 
-    public void test1() throws Exception {
-        System.out.println("test1");
+    public void test() throws Exception {
         UtilMailboxFactory mailboxFactory = new UtilMailboxFactory();
         try {
-            int _count = startReq1(mailboxFactory.createNonBlockingMailbox()).call();
+            int _count = startReq1(mailboxFactory.createAtomicMailbox()).call();
             assertEquals(5, _count);
         } finally {
             mailboxFactory.close();
@@ -28,8 +27,7 @@ public class AtomicTest extends TestCase {
             @Override
             public void processRequest(final Transport<Integer> _rp)
                     throws Exception {
-                final FifoRequestProcessor ap = FifoRequestProcessor
-                        .create(getMailbox().getMailboxFactory());
+                Mailbox mailbox = _mailbox.getMailboxFactory().createAtomicMailbox();
                 ResponseProcessor rc = new ResponseCounter(5, null,
                         new ResponseProcessor() {
                             @Override
@@ -38,23 +36,22 @@ public class AtomicTest extends TestCase {
                                 _rp.processResponse(count);
                             }
                         });
-                ap.atomicReq(aReq(ap, 1)).send(_mailbox, rc);
-                ap.atomicReq(aReq(ap, 2)).send(_mailbox, rc);
-                ap.atomicReq(aReq(ap, 3)).send(_mailbox, rc);
-                ap.atomicReq(aReq(ap, 4)).send(_mailbox, rc);
-                ap.atomicReq(aReq(ap, 5)).send(_mailbox, rc);
+                aReq(mailbox, 1).send(_mailbox, rc);
+                aReq(mailbox, 2).send(_mailbox, rc);
+                aReq(mailbox, 3).send(_mailbox, rc);
+                aReq(mailbox, 4).send(_mailbox, rc);
+                aReq(mailbox, 5).send(_mailbox, rc);
             }
         };
     }
 
-    Request<Void> aReq(final FifoRequestProcessor ap, final int msg) {
-        final Mailbox mailbox = ap.getMailbox();
-        return new Request<Void>(mailbox) {
+    Request<Void> aReq(final Mailbox _mailbox, final int msg) {
+        return new Request<Void>(_mailbox) {
             @Override
             public void processRequest(final Transport<Void> _rp)
                     throws Exception {
-                Delay delay = new Delay(mailbox.getMailboxFactory());
-                delay.sleepReq(100 - (msg * 20)).send(mailbox,
+                Delay delay = new Delay(_mailbox.getMailboxFactory());
+                delay.sleepReq(100 - (msg * 20)).send(_mailbox,
                         new ResponseProcessor<Void>() {
                             @Override
                             public void processResponse(Void response)
@@ -65,30 +62,6 @@ public class AtomicTest extends TestCase {
                                 _rp.processResponse(null);
                             }
                         });
-            }
-        };
-    }
-
-    public void test2() throws Exception {
-        System.out.println("test2");
-        UtilMailboxFactory mailboxFactory = new UtilMailboxFactory();
-        try {
-            final FifoRequestProcessor fp = new FifoRequestProcessor();
-            fp.initialize(mailboxFactory.createNonBlockingMailbox(fp));
-            fp.atomicReq(bReq(fp.getMailbox())).call();
-        } catch (UnsupportedOperationException uoe) {
-            mailboxFactory.close();
-            return;
-        }
-        throw new IllegalStateException();
-    }
-
-    Request<Void> bReq(final Mailbox _mailbox) {
-        return new Request<Void>(_mailbox) {
-            @Override
-            public void processRequest(Transport<Void> responseProcessor)
-                    throws Exception {
-                throw new UnsupportedOperationException("it happen");
             }
         };
     }
