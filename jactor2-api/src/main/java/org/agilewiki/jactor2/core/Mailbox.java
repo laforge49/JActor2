20e5f486636fb37cf11ac8e72c6e5555766b5472
Bill La Forge <laforge49@gmail.com>
2013-07-31 11:05:45 +0530

    folded _Mailbox into Mailbox

--- jactor2-api/src/main/java/org/agilewiki/jactor2/core/Mailbox.java
+++ jactor2-api/src/main/java/org/agilewiki/jactor2/core/Mailbox.java
@@ -1,5 +1,7 @@
 package org.agilewiki.jactor2.core;
 
+import org.slf4j.Logger;
+
 /**
  * A mailbox implements an inbox for incoming messages (events/requests)
  * and buffers outgoing messages by destination mailbox.
@@ -7,7 +9,7 @@ package org.agilewiki.jactor2.core;
  * While a mailbox has a non-empty inbox, it has an assigned thread that processes
  * the contents of its inbox. And only one message is processed at a time.
  */
-public interface Mailbox extends Runnable, _Mailbox {
+public interface Mailbox extends Runnable, MessageSource {
 
     /**
      * Returns the mailbox factory.
@@ -53,4 +55,65 @@ public interface Mailbox extends Runnable, _Mailbox {
      *         default exception handler was in effect.
      */
     ExceptionHandler setExceptionHandler(final ExceptionHandler exceptionHandler);
+
+    /**
+     * Add a message directly to the input queue of a Mailbox.
+     *
+     * @param message A message.
+     * @param local   True when the current thread is bound to the mailbox.
+     */
+    void unbufferedAddMessages(final Message message, final boolean local)
+            throws Exception;
+
+    /**
+     * Buffers a message in the sending mailbox for sending later.
+     *
+     * @param message Message to be buffered.
+     * @param target  The mailbox that should eventually receive this message
+     * @return True if the message was buffered.
+     */
+    boolean buffer(final Message message, final Mailbox target);
+
+    /**
+     * Returns true, if this mailbox is currently processing messages.
+     */
+    boolean isRunning();
+
+    /**
+     * Returns the message currently being processed.
+     *
+     * @return The message currently being processed.
+     */
+    Message getCurrentMessage();
+
+    /**
+     * The current exception handler.
+     *
+     * @return The current exception handler, or null.
+     */
+    ExceptionHandler getExceptionHandler();
+
+    /**
+     * Returns the mailbox logger.
+     *
+     * @return The mailbox logger.
+     */
+    Logger getLogger();
+
+    /**
+     * Identify the message currently being processed.
+     *
+     * @param message The message currently being processed.
+     */
+    void setCurrentMessage(Message message);
+
+    /**
+     * Signals the start of a request.
+     */
+    void requestBegin();
+
+    /**
+     * Signals that a request has completed.
+     */
+    void requestEnd();
 }
