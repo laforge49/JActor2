d12d90e5ce60e65ae560d4693f0ca193a407d7e3
Bill LaForge <laforge49@gmail.com>
2013-08-12 17:25:31 +0530

    Added sample to ExceptionHandler javadocs

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/messaging/ExceptionHandler.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/messaging/ExceptionHandler.java
@@ -23,6 +23,115 @@ package org.agilewiki.jactor2.core.messaging;
  * Exception handlers can also return a result, providing they have access to the appropriate
  * ResponseProcessor object.
  * </p>
+ * <h3>Sample Usage:</h3>
+ * <pre>
+ * import org.agilewiki.jactor2.core.ActorBase;
+ * import org.agilewiki.jactor2.core.context.JAContext;
+ * import org.agilewiki.jactor2.core.mailbox.Mailbox;
+ * import org.agilewiki.jactor2.core.mailbox.NonBlockingMailbox;
+ *
+ * public class ExceptionHandlerSample {
+ *
+ *     public static void main(final String[] _args) throws Exception {
+ *
+ *         //A context with two threads.
+ *         final JAContext jaContext = new JAContext(2);
+ *
+ *         try {
+ *
+ *             //Create an ExceptionActor.
+ *             ExceptionActor exceptionActor = new ExceptionActor(new NonBlockingMailbox(jaContext));
+ *
+ *             try {
+ *                 //Create and call an exception request.
+ *                 exceptionActor.exceptionReq().call();
+ *                 System.out.println("can not get here");
+ *             } catch (IllegalStateException ise) {
+ *                 System.out.println("got first IllegalStateException, as expected");
+ *             }
+ *
+ *             //Create an ExceptionHandlerActor.
+ *             ExceptionHandlerActor exceptionHandlerActor =
+ *                     new ExceptionHandlerActor(exceptionActor, new NonBlockingMailbox(jaContext));
+ *             //Create a test request, call it and print the results.
+ *             System.out.println(exceptionHandlerActor.testReq().call());
+ *
+ *         } finally {
+ *             //shutdown the context
+ *             jaContext.close();
+ *         }
+ *     }
+ * }
+ *
+ * //An actor with a request that throws an exception.
+ * class ExceptionActor extends ActorBase {
+ *
+ *     //Create an ExceptionActor.
+ *     ExceptionActor(final Mailbox _mailbox) throws Exception {
+ *         initialize(_mailbox);
+ *     }
+ *
+ *     //Returns an exception request.
+ *     Request&lt;Void&gt; exceptionReq() {
+ *         return new Request&lt;Void&gt;(getMailbox()) {
+ *
+ *             {@literal @}Override
+ *             public void processRequest(final Transport&lt;Void&gt; _transport) throws Exception {
+ *                 throw new IllegalStateException(); //Throw an exception when the request is processed.
+ *             }
+ *         };
+ *     }
+ * }
+ *
+ * //An actor with an exception handler.
+ * class ExceptionHandlerActor extends ActorBase {
+ *
+ *     //An actor with a request that throws an exception.
+ *     private final ExceptionActor exceptionActor;
+ *
+ *     //Create an exception handler actor with a reference to an exception actor.
+ *     ExceptionHandlerActor(final ExceptionActor _exceptionActor, final Mailbox _mailbox) throws Exception {
+ *         exceptionActor = _exceptionActor;
+ *         initialize(_mailbox);
+ *     }
+ *
+ *     //Returns a test request.
+ *     Request&lt;String&gt; testReq() {
+ *         return new Request&lt;String&gt;(getMailbox()) {
+ *
+ *             {@literal @}Override
+ *             public void processRequest(final Transport&lt;String&gt; _transport) throws Exception {
+ *
+ *                 //Create and assign an exception handler.
+ *                 getMailbox().setExceptionHandler(new ExceptionHandler() {
+ *                     {@literal @}Override
+ *                     public void processException(final Throwable _throwable) throws Throwable {
+ *                         if (_throwable instanceof IllegalStateException) {
+ *                             //Returns a result if an IllegalStateException was thrown.
+ *                             _transport.processResponse("got IllegalStateException, as expected");
+ *                         } else //Otherwise rethrow the exception.
+ *                             throw _throwable;
+ *                     }
+ *                 });
+ *
+ *                 //Create an exception request and send it to the exception actor for processing.
+ *                 //The thrown exception is then caught by the assigned exception handler.
+ *                 exceptionActor.exceptionReq().send(getMailbox(), new ResponseProcessor&lt;Void&gt;() {
+ *                     {@literal @}Override
+ *                     public void processResponse(final Void _response) throws Exception {
+ *                         _transport.processResponse("can not get here");
+ *                     }
+ *                 });
+ *             }
+ *         };
+ *     }
+ *
+ * }
+ *
+ * Output:
+ * got first IllegalStateException, as expected
+ * got IllegalStateException, as expected
+ * </pre>
  */
 public interface ExceptionHandler {
     /**
