e371485abb6879c67b3abf37007e465797c6dce9
Bill LaForge <laforge49@gmail.com>
2013-08-14 11:33:00 +0530

    finished the service sample

--- jactor2-core/src/samples/java/org/agilewiki/jactor2/core/messaging/ServiceSample.java
+++ jactor2-core/src/samples/java/org/agilewiki/jactor2/core/messaging/ServiceSample.java
@@ -5,53 +5,76 @@ import org.agilewiki.jactor2.core.context.JAContext;
 import org.agilewiki.jactor2.core.mailbox.Mailbox;
 import org.agilewiki.jactor2.core.mailbox.NonBlockingMailbox;
 
+//Exploring the use of multiple contexts.
 public class ServiceSample {
 
     public static void main(final String[] _args) throws Exception {
 
-        JAContext serviceContext = new JAContext(1);
+        //Application context with 1 thread.
         final JAContext applicationContext = new JAContext(1);
+
+        //Create a service actor that uses its own context.
+        Service service = new Service();
+
         try {
-            Service service = new Service(new NonBlockingMailbox(serviceContext));
+            //Test the delay echo request on the service actor.
             System.out.println(service.delayEchoReq(1, "1 (Expected)").call());
-            serviceContext.close();
+
+            //close the context used by the service actor.
+            service.getMailbox().getJAContext().close();
             try {
+                //Try using delay echo request with the context closed.
                 System.out.println(service.delayEchoReq(1, "(Unexpected)").call());
             } catch (ServiceClosedException sce) {
+                //The ServiceClosedException is now thrown because the context is closed.
                 System.out.println("Exception as expected");
             }
-            serviceContext = new JAContext(1);
-            service = new Service(new NonBlockingMailbox(serviceContext));
+
+            //Create a new service actor that uses its own context.
+            service = new Service();
+            //Create an application actor based on the application context
+            //and with a reference to the service actor.
             final ServiceApplication serviceApplication =
                     new ServiceApplication(service, new NonBlockingMailbox(applicationContext));
+            //Start a delay echo service request using the application actor.
             EchoReqState echoReqState = serviceApplication.echoReq(1, "2 (Expected)").call();
+            //Print the results of the delay echo service request.
             System.out.println(serviceApplication.echoResultReq(echoReqState).call());
+
+            //Start a second delay echo service request using the application actor.
             EchoReqState echoReqState2 = serviceApplication.echoReq(1, "(Unexpected)").call();
+            //Close the service context while the delay echo service request is still sleeping.
             serviceApplication.closeServiceReq().call();
+            //The results should now show that an exception was thrown.
             System.out.println(serviceApplication.echoResultReq(echoReqState2).call());
         } finally {
-            serviceContext.close();
-            applicationContext.close();
+            service.getMailbox().getJAContext().close(); //Close the service context.
+            applicationContext.close(); //Close the application context.
         }
 
     }
 }
 
+//A service actor that runs on its own context.
 class Service extends ActorBase {
 
-    Service(final Mailbox _mailbox) throws Exception {
-        initialize(_mailbox);
+    Service() throws Exception {
+        //Create a mailbox on a new context with 1 thread.
+        initialize(new NonBlockingMailbox(new JAContext(1)));
     }
 
+    //Returns a delay echo request.
     Request<String> delayEchoReq(final int _delay, final String _text) {
         return new Request<String>(getMailbox()) {
             @Override
             public void processRequest(Transport<String> _transport) throws Exception {
+                //Sleep a bit so that the request does not complete too quickly.
                 try {
                     Thread.sleep(_delay);
                 } catch (InterruptedException e) {
                     return;
                 }
+                //Echo the text back in the response.
                 _transport.processResponse("Echo: " + _text);
             }
         };
@@ -59,32 +82,58 @@ class Service extends ActorBase {
 
 }
 
+//Holds the state of a service application echo request.
 class EchoReqState {
+    //Not null when an echoResultRequest was received before
+    // the result of the matching service delay echo request.
     Transport<String> transport;
+
+    //Not null when the result of the service delay echo request is received
+    //before the matching echoResultRequest.
     String response;
 }
 
+//An actor with a context that is different than the context of the service actor.
 class ServiceApplication extends ActorBase {
+
+    //The service actor, which operates in a different context.
     private final Service service;
 
+    //Create a service application actor with a reference to a service actor.
     ServiceApplication(final Service _service, final Mailbox _mailbox) throws Exception {
         service = _service;
         initialize(_mailbox);
     }
+
+    //Returns an application echo request.
+    //The echo request is used to initiate a service delay echo request.
+    //And the response returned by the echo request is state data needed to manage the
+    //delivery of the response from the service delay echo request.
     Request<EchoReqState> echoReq(final int _delay, final String _text) {
         return new Request<EchoReqState>(getMailbox()) {
             @Override
             public void processRequest(Transport<EchoReqState> _transport) throws Exception {
+
+                //State data needed to manage the delivery of the response from
+                //the service delay echo request.
                 final EchoReqState echoReqState = new EchoReqState();
+
+                //Establish an exception handler which traps a ServiceClosedException and
+                //returns a notification that the exception occurred as a result.
                 getMailbox().setExceptionHandler(new ExceptionHandler() {
                     @Override
                     public void processException(Throwable throwable) throws Throwable {
                         if (throwable instanceof ServiceClosedException) {
                             String response = "Exception as expected";
-                            if (echoReqState.transport == null)
+                            if (echoReqState.transport == null) {
+                                //No echo result request has yet been received,
+                                //so save the response for later.
                                 echoReqState.response = response;
-                            else
+                            } else {
+                                //An echo result request has already been received,
+                                //so now is the time to return the response.
                                 echoReqState.transport.processResponse(response);
+                            }
                         } else
                             throw throwable;
                     }
@@ -93,9 +142,14 @@ class ServiceApplication extends ActorBase {
                     @Override
                     public void processResponse(String response) throws Exception {
                         if (echoReqState.transport == null)
+                            //No echo result request has yet been received,
+                            //so save the response for later.
                             echoReqState.response = response;
-                        else
+                        else {
+                            //An echo result request has already been received,
+                            //so now is the time to return the response.
                             echoReqState.transport.processResponse(response);
+                        }
                     }
                 });
                 _transport.processResponse(echoReqState);
@@ -103,24 +157,34 @@ class ServiceApplication extends ActorBase {
         };
     }
 
+    //Returns a close service request.
     Request<Void> closeServiceReq() {
         return new Request<Void>(getMailbox()) {
             @Override
             public void processRequest(Transport<Void> _transport) throws Exception {
+                //Close the context of the service actor.
                 service.getMailbox().getJAContext().close();
                 _transport.processResponse(null);
             }
         };
     }
 
+    //Returns an echo result request.
+    //An echo result request returns the response from the service delay echo request
+    //associated with the given echo request state.
     Request<String> echoResultReq(final EchoReqState _echoReqState) {
         return new Request<String>(getMailbox()) {
             @Override
             public void processRequest(Transport<String> _transport) throws Exception {
-                if (_echoReqState.response == null)
+                if (_echoReqState.response == null) {
+                    //There is as yet no response from the associated service delay echo request,
+                    //so save the transport of this request for subsequent delivery of that belated response.
                     _echoReqState.transport = _transport;
-                else
+                } else {
+                    //The response from the associated service delay echo request is already present,
+                    //so return that response now.
                     _transport.processResponse(_echoReqState.response);
+                }
             }
         };
     }
