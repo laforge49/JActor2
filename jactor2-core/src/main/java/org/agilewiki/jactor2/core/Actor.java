5571f98bb4a736bae21d2f5b930918d4c56b9acf
Bill LaForge <laforge49@gmail.com>
2013-08-17 16:45:28 +0530

    Updated Actor

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/Actor.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/Actor.java
@@ -4,29 +4,29 @@ import org.agilewiki.jactor2.core.processing.MessageProcessor;
 
 /**
  * <p>
- * Actors must implement the Actor interface to provide access to their processing.
+ * Actors must implement the Actor interface to provide access to their message processor.
  * </p>
  * <h3>Sample Usage:</h3>
  * <pre>
  * public class ActorSample implements Actor {
- *     private final MessageProcessor processing;
+ *     private final MessageProcessor processor;
  *
- *     ActorSample(final MessageProcessor _mailbox) {
- *         processing = _mailbox;
+ *     ActorSample(final MessageProcessor _processor) {
+ *         processor = _processor;
  *     }
  *
  *     {@literal @}Override
  *     public final MessageProcessor getMessageProcessor() {
- *         return processing;
+ *         return processor;
  *     }
  * }
  * </pre>
  */
 public interface Actor {
     /**
-     * Returns the processing associated with this Actor.
+     * Returns the message processor associated with this Actor.
      *
-     * @return The actor's processing.
+     * @return The actor's message processor.
      */
     public MessageProcessor getMessageProcessor();
 }
