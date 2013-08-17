b780f5abe49d85638a80ebc74298b97b9d6abf74
Bill LaForge <laforge49@gmail.com>
2013-08-17 16:56:15 +0530

    updated atomic message processor

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/processing/AtomicMessageProcessor.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/processing/AtomicMessageProcessor.java
@@ -5,24 +5,24 @@ import org.agilewiki.jactor2.core.context.MigrationException;
 import org.agilewiki.jactor2.core.messaging.Message;
 
 /**
- * A processing which processes each request to completion, and which should be used by actors
+ * A message processor which processes each request to completion, and which should be used by actors
  * which perform long computations, I/O, or otherwise block the thread.
  * <p>
  * For thread safety, the processing of each message is atomic, but when the processing of a
  * message results in the sending of a request, other messages may be processed before a
- * response to that request is received. However, an atomic processing will not process another
+ * response to that request is received. However, an atomic message processor will not process another
  * request until a response is returned for the prior request. This does not however preclude
  * the processing of event messages.
  * </p>
  * <p>
- * Request/Response messages which are destined to a different processing are buffered rather
+ * Request/Response messages which are destined to a different message processor are buffered rather
  * than being sent immediately. These messages are disbursed to their destinations when the
  * processing of each incoming message is complete.
  * </p>
  * <p>
  * When the last block of buffered messages is being disbursed, if the destination is not
- * a thread-bound processing, the destination processing has no associated thread and the
- * context of the current processing is the same as the destination processing, then the
+ * a thread-bound message processor, the destination message processor has no associated thread and the
+ * context of the current message processor is the same as the destination message processor, then the
  * current thread migrates with the message block. By this means the message block is
  * often kept in the hardware thread's high-speed memory cache, which means much faster
  * execution.
@@ -34,9 +34,9 @@ import org.agilewiki.jactor2.core.messaging.Message;
 public class AtomicMessageProcessor extends UnboundMessageProcessor {
 
     /**
-     * Create an atomic processing.
+     * Create an atomic message processor.
      *
-     * @param _jaContext The context of the processing.
+     * @param _jaContext The context of the message processor.
      */
     public AtomicMessageProcessor(JAContext _jaContext) {
         super(_jaContext, _jaContext.getInitialBufferSize(),
@@ -44,9 +44,9 @@ public class AtomicMessageProcessor extends UnboundMessageProcessor {
     }
 
     /**
-     * Create an atomic processing.
+     * Create an atomic message processor.
      *
-     * @param _jaContext The context of the processing.
+     * @param _jaContext The context of the message processor.
      * @param _onIdle    Object to be run when the inbox is emptied, or null.
      */
     public AtomicMessageProcessor(JAContext _jaContext, Runnable _onIdle) {
@@ -55,9 +55,9 @@ public class AtomicMessageProcessor extends UnboundMessageProcessor {
     }
 
     /**
-     * Create an atomic processing.
+     * Create an atomic message processor.
      *
-     * @param _jaContext             The context of the processing.
+     * @param _jaContext             The context of the message processor.
      * @param _initialOutboxSize     Initial size of the outbox for each unique message destination.
      * @param _initialLocalQueueSize The initial number of slots in the local queue.
      * @param _onIdle                Object to be run when the inbox is emptied, or null.
