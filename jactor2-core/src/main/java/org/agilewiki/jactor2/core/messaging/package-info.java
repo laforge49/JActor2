cffeed51c09cec0c07c4f0ff5f805b9de712a1f3
Bill LaForge <laforge49@gmail.com>
2013-08-17 11:30:51 +0530

    Updated messaging package info

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/messaging/package-info.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/messaging/package-info.java
@@ -8,30 +8,29 @@
  *     Event messages are created and passed using subclasses of the Event class.
  *     An event is not bound to an actor instance, so the same event can be used to
  *     send messages to any number of actors. And when passing an event message to an actor,
- *     the message is given to the target actor's processing immediately.
+ *     the message is given to the target actor's processing immediately, without buffering.
  * </p>
  * <h2>Request/Response Messages</h2>
  * <p>
  *     In contrast, request messages are created and passed using anonymous subclasses of
  *     the Request class, with these anonymous classes present within the actor where the
  *     request message is evaluated. Response messages are simply request messages to which a
- *     response has been assigned. Both request and response messages, when passed from an
- *     actor, are aggregated and sent in groups to reduce the overhead of passing individual
- *     messages.
+ *     response has been assigned. Request and response messages are aggregated and sent in
+ *     blocks to reduce the overhead of having to pass individual messages.
  * </p>
  * <h2>Exception Handling</h2>
  * <p>
- *     Exception handlers are used to process otherwise uncaught exception. But if there is no
+ *     Exception handlers are used to process otherwise uncaught exceptions. But if there is no
  *     exception handler, or if an exception is thrown by an exception handler, then the
  *     exception is returned as a response, though if the current message being processed is
  *     an event then the exception is simply logged. However, when a response message that
- *     holds an event is processed by the source processing, the exception is rethrown rather
- *     than passing it back to the application as a valid response.
+ *     holds an event is processed by the source message processor, the exception is rethrown
+ *     within the source actor rather than being pass back to the actor as a valid response.
  * </p>
  * <p>
- *     Before a processing processes an event or a request, the current exception handler is set
- *     to null. The application's message processing logic then has the option of assigning
- *     an exception handler via the MessageProcessor.setExceptionHandler method. When a request message
+ *     Before an event or a request is processed, the current exception handler is set
+ *     to null. The application logic specific to that event or request then has the option of assigning
+ *     an exception handler via the MessageProcessor.setExceptionHandler method. And when a request message
  *     is sent, the current exception handler is saved in the request message and subsequently
  *     restored when a response is received.
  * </p>
