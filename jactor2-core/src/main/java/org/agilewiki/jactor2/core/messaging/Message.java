47b9d133a974cbeba5bb80609d14ead8214fdaa0
Bill LaForge <laforge49@gmail.com>
2013-08-18 14:07:35 +0530

    Updated Message

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/messaging/Message.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/messaging/Message.java
@@ -4,7 +4,7 @@ import org.agilewiki.jactor2.core.processing.MessageProcessor;
 
 /**
  * Message wraps the user/application Request/Event which are queued in the
- * Actor's processing. The lightweight thread associated with the Actor's processing will process
+ * Actor's message processor's inbox. The lightweight thread associated with the Actor's message processor will process
  * the Message asynchronously.
  * <p>
  * Both Event and Request have private nested classes specific to their requirements.
@@ -14,11 +14,11 @@ import org.agilewiki.jactor2.core.processing.MessageProcessor;
 public interface Message extends AutoCloseable {
 
     /**
-     * Returns true when the response is to be sent to a processing created from a different
-     * processing factory.
+     * Returns true when the response is to be sent to a message processor created with a different
+     * context.
      *
-     * @return True when the response is to be sent to a processing created from a different
-     *         processing factory.
+     * @return True when the response is to be sent to a message processor created with a different
+     *         context.
      */
     boolean isForeign();
 
@@ -32,16 +32,16 @@ public interface Message extends AutoCloseable {
     /**
      * Execute the Event.processEvent or Request.processRequest method
      * of the event/request held by the message. This method is always called on the
-     * target processing's own thread.
+     * target message processor's own thread.
      *
-     * @param _targetMessageProcessor The processing whose thread is to evaluate the event/request.
+     * @param _targetMessageProcessor The message processor whose thread is to evaluate the event/request.
      */
     void eval(final MessageProcessor _targetMessageProcessor);
 
     /**
-     * Process the throwable on the current thread in the context of the active processing.
+     * Process the throwable on the current thread in the context of the active message processor.
      *
-     * @param _activeMessageProcessor The processing providing the context for processing the throwable.
+     * @param _activeMessageProcessor The message processor providing the context for processing the throwable.
      * @param _t                      The throwable to be processed.
      */
     void processThrowable(final MessageProcessor _activeMessageProcessor, final Throwable _t);
