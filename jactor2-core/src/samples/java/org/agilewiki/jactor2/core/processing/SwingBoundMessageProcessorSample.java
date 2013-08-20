74037b1c9d4c34075ce8b865a2197b531685e67a
Bill LaForge <laforge49@gmail.com>
2013-08-20 09:29:33 +0530

    Added context close to swing sample

--- jactor2-core/src/samples/java/org/agilewiki/jactor2/core/processing/SwingBoundMessageProcessorSample.java
+++ jactor2-core/src/samples/java/org/agilewiki/jactor2/core/processing/SwingBoundMessageProcessorSample.java
@@ -6,16 +6,22 @@ import org.agilewiki.jactor2.core.messaging.Request;
 import org.agilewiki.jactor2.core.messaging.Transport;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class SwingBoundMessageProcessorSample {
     public static void main(final String[] _args) throws Exception {
-        HelloWorld helloWorld = new HelloWorld(new SwingBoundMessageProcessor(new JAContext(0)));
+
+        //Create a context with 5 threads.
+        JAContext context = new JAContext(5);
+
+        HelloWorld helloWorld = new HelloWorld(new SwingBoundMessageProcessor(context));
         helloWorld.createAndShowReq().signal();
     }
 }
 
 class HelloWorld extends ActorBase {
-    HelloWorld(final SwingBoundMessageProcessor _messageProcessor) throws Exception{
+    HelloWorld(final SwingBoundMessageProcessor _messageProcessor) throws Exception {
         initialize(_messageProcessor);
     }
 
@@ -25,7 +31,19 @@ class HelloWorld extends ActorBase {
             public void processRequest(Transport<Void> _transport) throws Exception {
                 //Create and set up the window.
                 JFrame frame = new JFrame("HelloWorldSwing");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+                //Close context when window is closed.
+                frame.addWindowListener(new WindowAdapter() {
+                    @Override
+                    public void windowClosed(WindowEvent e) {
+                        try {
+                            getMessageProcessor().getJAContext().close();
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                });
 
                 //Add the ubiquitous "Hello World" label.
                 JLabel label = new JLabel("Hello World");
@@ -38,4 +56,5 @@ class HelloWorld extends ActorBase {
             }
         };
     }
+
 }
