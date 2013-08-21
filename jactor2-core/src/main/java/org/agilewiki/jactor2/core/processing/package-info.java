d2c678843972aa6ddada2bc6bd5658ee3260806c
Bill LaForge <laforge49@gmail.com>
2013-08-21 09:42:39 +0530

    Processing package info now covers SwingBoundMessageProcessor

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/processing/package-info.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/processing/package-info.java
@@ -1,5 +1,5 @@
 /**
- * <h1>Non-blocking, Atomic and Thread-bound Message Processors</h1>
+ * <h1>Non-blocking, Atomic, Thread-bound and Swing-bound Message Processors</h1>
  * <p>
  *     While MessageProcessors are used by the Event and Request classes to pass messages and process messages,
  *     the MessageProcessor interface itself include only a few methods for use by
@@ -7,9 +7,10 @@
  * </p>
  * <h2>Creating a MessageProcessor</h2>
  * <p>
- *     Actors can use 3 different classes of message processors: NonBlockingMessageProcessor, AtomicMessageProcessor
- *     and ThreadBoundMessageProcessor. MessageProcessor instances are easily created, with an instance of
- *     JAContext as a required parameter. Additional parameters can be also bepassed to the constructor
+ *     Actors can use 4 different classes of message processors: NonBlockingMessageProcessor, AtomicMessageProcessor,
+ *     ThreadBoundMessageProcessor and SwingBoundMessageProcessor.
+ *     MessageProcessor instances are easily created, with an instance of
+ *     JAContext as a required parameter. Additional parameters can be also be passed to the constructor
  *     for configuring a message processor:
  * </p>
  * <ul>
@@ -23,14 +24,13 @@
  * </li>
  * <li>
  *     <b>Runnable onIdle</b> The onIdle.run method is called when the inbox becomes empty.
- *     (This parameter does not apply to ThreadBoundMessageProcessor.)
+ *     (This parameter does not apply to ThreadBoundMessageProcessor nor to SwingBoundMessageProcessor.)
  * </li>
  * <li>
  *     <b>Runnable boundProcessor</b> The boundProcessor.run method is called when a
  *     thread-bound processor has messages that need processing. As a result of invoking the
  *     boundProcessor.run method, the ThreadBoundMessageProcessor.run method be invoked in turn by the thread that
- *     the processing is bound to. (This parameter does not apply to NonBlockingMessageProcessor nor to
- *     AtomicMessageProcessor.)
+ *     the processing is bound to. (This parameter applies only to ThreadBoundMessageProcessor.)
  * </li>
  * </ul>
  */
