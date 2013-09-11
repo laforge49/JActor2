00422972d8d5b1b29aab17300898b76c97e044c4
Bill La Forge <laforge49@gmail.com>
2013-09-11 09:22:48 +0530

    javadocs for non-blocking and isolation actors.

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/IsolationActor.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/IsolationActor.java
@@ -3,7 +3,19 @@ package org.agilewiki.jactor2.core;
 import org.agilewiki.jactor2.core.processing.IsolationMessageProcessor;
 import org.agilewiki.jactor2.core.threading.ModuleContext;
 
+/**
+ * An isolation actor processes requests from other actors one at a time,
+ * starting a new request only when a result is returned for the previous
+ * one.
+ */
 public class IsolationActor extends ActorBase {
+
+    /**
+     * Create an isolation actor.
+     *
+     * @param _moduleContext    A set of resources, including a thread pool, for use
+     *                          by message processors and their actors.
+     */
     public IsolationActor(final ModuleContext _moduleContext) throws Exception {
         initialize(new IsolationMessageProcessor(_moduleContext));
     }
--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/NonBlockingActor.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/NonBlockingActor.java
@@ -2,8 +2,19 @@ package org.agilewiki.jactor2.core;
 
 import org.agilewiki.jactor2.core.processing.NonBlockingMessageProcessor;
 
+/**
+ * An actor which does not perform long computations nor otherwise block the thread.
+ */
 public class NonBlockingActor extends ActorBase {
-    public NonBlockingActor(final NonBlockingMessageProcessor _nonBlockingMessageProcessor) throws Exception {
+
+    /**
+     * Create a non-blocking actor.
+     *
+     * @param _nonBlockingMessageProcessor A message processor for actors which process messages
+     *                                     quickly and without blocking the thread.
+     */
+    public NonBlockingActor(final NonBlockingMessageProcessor _nonBlockingMessageProcessor)
+            throws Exception {
         initialize(_nonBlockingMessageProcessor);
     }
 }
