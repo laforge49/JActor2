34b4888b5486bddad1919a274d6264f59fbbfeb2
Bill La Forge <laforge49@gmail.com>
2013-09-11 19:19:14 +0530

    added sample to PrinterAdjunct

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/PrinterAdjunct.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/PrinterAdjunct.java
@@ -4,6 +4,32 @@ package org.agilewiki.jactor2.core;
  * A PrinterAdjunct can be used to print multiple lines
  * without having any other text being interleaved by other actors
  * using the same Printer.
+ * </p>
+ * <h3>Sample Usage:</h3>
+ * <pre>
+ * //Prints a banner without allowing any intervening lines.
+ * public class PrinterAdjunctSample extends PrinterAdjunct {
+ *
+ *     public PrinterAdjunctSample(Printer _printer) throws Exception {
+ *         psuper(_printer);
+ *     }
+ *
+ *     // Returns a request to print a Hi! banner.
+ *     public SyncRequest&lt;Void&gt; hiSReq() {
+ *         return new SyncRequest&lt;Void&gt;(getMessageProcessor()) {
+ *             {@literal @}Override
+ *             public Void processSyncRequest() throws Exception {
+ *                 printer.printlnSReq("*********").local(messageProcessor);
+ *                 printer.printlnSReq("*       *").local(messageProcessor);
+ *                 printer.printlnSReq("*  Hi!  *").local(messageProcessor);
+ *                 printer.printlnSReq("*       *").local(messageProcessor);
+ *                 printer.printlnSReq("*********").local(messageProcessor);
+ *                 return null;
+ *             }
+ *         };
+ *     }
+ * }
+ * </pre>
  */
 public class PrinterAdjunct extends ActorBase {
     /**
