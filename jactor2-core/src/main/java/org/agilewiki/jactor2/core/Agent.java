ec0f36649b0383784dbbbffc60ab024d1469239f
Bill La Forge <laforge49@gmail.com>
2013-09-13 11:45:37 +0530

    updated Agent javadocs

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/Agent.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/Agent.java
@@ -5,8 +5,8 @@ import org.agilewiki.jactor2.core.messaging.AsyncRequest;
 /**
  * An Agent is a blade with a StartAReq and which is dynamically added to a Reactor
  * to interact with the blades of that Reactor. One of the advantages of an Agent is
- * to reduce the number of requests that pass between threads. The other is to perform
- * multiple requests against the other blades in that reactor in isolation from other requests.
+ * to reduce the number of requests that pass between threads. The other can be to perform
+ * multiple local requests against the other blades in that reactor in isolation from other requests.
  * </p>
  * Any needed parameters are not passed to the startAReq, but are instead are typically
  * passed in the constructor of the subclasses of Agent.
