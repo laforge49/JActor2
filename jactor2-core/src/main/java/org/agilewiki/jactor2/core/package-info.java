800e8468779455e7d2a73fd575ed6a9cf75f1d96
Bill La Forge <laforge49@gmail.com>
2013-09-14 17:10:34 +0530

    updated core package info

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/package-info.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/package-info.java
@@ -1,27 +1,33 @@
 /**
- * <h1>Blades, Reactors and Facilities</h1>
+ * <h1>Messages, Blades, Reactors and Facilities</h1>
  * <p>
- *     The JActor architecture is divided into 4 parts: actors, processing, threading and messaging.
+ *     The JActor architecture is divided into 4 parts: messages, blades, reactors and facilities.
  * </p>
- * <h2>Actors</h2>
+ * <h2>Messaging</h2>
  * <p>
- *     Every actor has a reactor which processes the messages sent to that actor. Thread safety
- *     for an actor is achieved by processing only one message at a time.
- * </p>
- * <p>
- *     To be an actor, an object need only implement the Blade interface. And the Blade
- *     interface has a single method, getReactor(), that returns the actor's reactor.
+ *     Both 1-way messages (events) and 2-way messages (request/response) are supported.
  * </p>
  * <p>
- *     More than one actor can share the same reactor. And because a reactor only processes
- *     one message at a time, actors which share a processing can safely call each other's
- *     methods. So the actors which share the same processing can be thought of as being part of
- *     a larger composite actor.
+ *     Events are reusable and generally immutable. This allows a single event to be passed to multiple
+ *     blades. Event messages however must carry a reference to the target blade, so events are wrapped
+ *     in an event message before being passed to the target reactor for delivery.
  * </p>
- * <h2>Processing</h2>
  * <p>
- *     A reactor receives and subsequently processes the messages for one or more actors, processing those
- *     messages one at a time. A reactor can also buffer messages being sent to other actors,
+ *     In contrast, requests are not reusable. They are themselves messages which are passed to a
+ *     target reactor and then (usually) returned with a result.
+ * </p>
+ * <h2>Blades</h2>
+ * <p>
+ *     Blades are the application logic. Blades define requests, create the requests for the blades
+ *     they wish to send the requests too, send requests, process requests and send and process events.
+ *     Blades also define and assign exception handlers for the messages they receive.
+ *     Every blade has a reactor which processes the messages sent to that blade and for sending requests
+ *     to other blades. Thread safety for a blade is achieved by the reactor, which processes only only one message at a time.
+ * </p>
+ * <h2>Reactors</h2>
+ * <p>
+ *     A reactor receives and subsequently processes the messages for one or more blades, processing those
+ *     messages one at a time. A reactor can also buffer messages being sent to other blades,
  *     and then sends all the messages for each destination as a group to reduce the overhead
  *     of message passing.
  * </p>
@@ -29,35 +35,20 @@
  *     A reactor has an inbox, which is a queue of received messages, and an outbox that has a send buffer
  *     for each destination.
  * </p>
- * <h2>Threading</h2>
+ * <h2>Facilities</h2>
  * <p>
  *     A facility is used to assign threads to reactors that have an inbox that is not empty,
  *     with at most one thread being assigned to any given reactor.
  * </p>
  * <p>
  *     There can be more than one facility within a program--which is important when working
- *     with OSGi. Each facility has a property set and an independent lifecycle. When messages are
- *     sent to a facility that is closed, an exception is raised in the sending actor.
+ *     with OSGi. Each facility has a property set and an independent lifecycle. When messages are or have been
+ *     sent to a facility that is closed, an exception is raised in the originating blade.
  * </p>
  * <p>
  *     Multiple facilities can also be important when a Swing program has multiple frames that
  *     have associated background tasks. If a frame has its own facility that is closed when
  *     the frame is closed, all the background activity associated with that frame is terminated.
- * </p>
- * <h2>Messaging</h2>
- * <p>
- *     Both 1-way messages (events) and 2-way messages (request/response) are supported.
- *     Uncaught exceptions which occur while processing a request are raised in the actor that sent that request.
- * </p>
- * <p>
- *     All messages are placed in the actor's inbox when received and are processed
- *     in the order received. But there are two exceptions to this. First, messages from
- *     actors with the same reactor are given preference over messages from other actors.
- *     </p>
- *     <p>
- *     The second exception to message ordering is when an isolation reactor is used, as an
- *     isolation reactor gives preference to events and responses over requests and in any case does not
- *     process a request until a response to the prior request has been sent.
  * </p>
  */
 package org.agilewiki.jactor2.core;
