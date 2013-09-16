a2d467c7c6b54418bde8d314f60fec43d3d5dd70
Bill La Forge <laforge49@gmail.com>
2013-09-16 11:42:58 +0530

    updated hello world

--- tutorials/core/HelloWorld.java
+++ tutorials/core/HelloWorld.java
@@ -1,17 +1,15 @@
-import org.agilewiki.jactor2.core.blades.*;
-import org.agilewiki.jactor2.core.facilities.*;
-import org.agilewiki.jactor2.core.messages.*;
-import org.agilewiki.jactor2.core.reactors.*;
+import org.agilewiki.jactor2.core.blades.misc.Printer;
+import org.agilewiki.jactor2.core.facilities.Facility;
+import org.agilewiki.jactor2.core.messages.SyncRequest;
+import org.agilewiki.jactor2.core.reactors.Reactor;
 
 public class HelloWorld {
     public static void main(final String[] _args) throws Exception {
         Facility facility = new Facility();
         try {
-            NonBlockingReactor reactor = new NonBlockingReactor(facility);
-            HelloWorldBlade helloWorldBlade = new HelloWorldBlade(reactor);
-            AsyncRequest<String> getGreetingAReq = helloWorldBlade.getGreetingAReq();
-            String response = getGreetingAReq.call();
-            System.out.println(response);
+            Printer printer = new Printer(facility);
+            SyncRequest<Void> printRequest = printer.printlnSReq("Hello World!");
+            printRequest.call();
         } finally {
             facility.close();
         }
