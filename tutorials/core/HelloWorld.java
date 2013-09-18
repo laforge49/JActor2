0be816e6f42329feda2a5a7fd90fc9dffa91ad05
Bill La Forge <laforge49@gmail.com>
2013-09-18 09:43:16 +0530

    updated HelloWorld to use the revised printer

--- tutorials/core/HelloWorld.java
+++ tutorials/core/HelloWorld.java
@@ -1,12 +1,14 @@
 import org.agilewiki.jactor2.core.blades.misc.Printer;
 import org.agilewiki.jactor2.core.facilities.Facility;
 import org.agilewiki.jactor2.core.messages.SyncRequest;
+import org.agilewiki.jactor2.core.reactors.IsolationReactor;
 
 public class HelloWorld {
     public static void main(final String[] _args) throws Exception {
         Facility facility = new Facility();
         try {
-            Printer printer = new Printer(facility);
+            IsolationReactor isolationReactor = new IsolationReactor(facility);
+            Printer printer = new Printer(isolationReactor);
             SyncRequest<Void> printRequest = printer.printlnSReq("Hello World!");
             printRequest.call();
         } finally {
