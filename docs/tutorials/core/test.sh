2eb84ec4d0bdd49b05fedf7fd69aaa602f12c143
Bill La Forge <laforge49@gmail.com>
2013-09-21 14:23:05 +0530

    version 0.3.2

--- docs/tutorials/core/test.sh
+++ docs/tutorials/core/test.sh
@@ -1,3 +1,3 @@
 rm -fr *.class
-javac -cp jactor2-core-0.3.1.jar:slf4j-api-1.7.5.jar *.java
-java -cp jactor2-core-0.3.1.jar:slf4j-api-1.7.5.jar:slf4j-simple-1.7.5.jar:. $1
+javac -cp jactor2-core-0.3.2.jar:slf4j-api-1.7.5.jar *.java
+java -cp jactor2-core-0.3.2.jar:slf4j-api-1.7.5.jar:slf4j-simple-1.7.5.jar:. $1
