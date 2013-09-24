d97413e5ad5b83b3fa518568e06bc1637c1697d8
Bill La Forge <laforge49@gmail.com>
2013-09-24 19:59:15 +0530

    new Echo.java

---
+++ docs/tutorials/core/Echo.java
@@ -0,0 +1,47 @@
+import org.agilewiki.jactor2.core.blades.BladeBase;
+import org.agilewiki.jactor2.core.blades.misc.Delay;
+import org.agilewiki.jactor2.core.facilities.Facility;
+import org.agilewiki.jactor2.core.messages.AsyncRequest;
+import org.agilewiki.jactor2.core.messages.AsyncResponseProcessor;
+import org.agilewiki.jactor2.core.messages.SyncRequest;
+import org.agilewiki.jactor2.core.reactors.IsolationReactor;
+import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
+import org.agilewiki.jactor2.core.reactors.Reactor;
+
+public class Echo extends BladeBase {
+    public Echo(final Reactor _reactor) throws Exception {
+        initialize(_reactor);
+    }
+    
+    public AsyncRequest<String> echoAReq(final String sound) {
+        return new AsyncBladeRequest<String>() {
+            AsyncRequest<String> dis = this;
+            @Override
+            protected void processAsyncRequest() throws Exception {
+                Reactor myReactor = getReactor();
+                Facility myFacility = myReactor.getFacility();
+                Delay delay = new Delay(new IsolationReactor(myFacility));
+                SyncRequest<Void> sleepSReq = delay.sleepSReq(2000);
+                send(sleepSReq, new AsyncResponseProcessor<Void>() {
+                    @Override
+                    public void processAsyncResponse(final Void _response) throws Exception {
+                        dis.processAsyncResponse(sound);
+                    }
+                });
+            }
+        };
+    }
+    
+    public static void main(final String[] _args) throws Exception {
+        Facility facility = new Facility();
+        try {
+            Echo echo = new Echo(new NonBlockingReactor(facility));
+            AsyncRequest<String> echoAReq = echo.echoAReq("Hello...");
+            String response = echoAReq.call();
+            if (!"Hello...".equals(response))
+                throw new IllegalStateException("invalid response: " + response);
+        } finally {
+            facility.close();
+        }
+    }
+}
