7911750d8aba13d6b464ac7b9dc3021614394f1a
Bill La Forge <laforge49@gmail.com>
2013-09-28 10:27:01 +0530

    Monster double try fix, links to other pages

--- docs/tutorials/core/JvmSendSpeedReport.java
+++ docs/tutorials/core/JvmSendSpeedReport.java
@@ -8,20 +8,23 @@ public class JvmSendSpeedReport {
     public static void main(final String[] _args) throws Exception {
         final long count = 1000000L;
         Facility facility1 = new Facility();
-        Facility facility2 = new Facility();
         try {
-            Ponger ponger = new Ponger(new NonBlockingReactor(facility1));
-            Pinger pinger = new Pinger(new NonBlockingReactor(facility2), ponger);
-            AsyncRequest<Void> loopAReq = pinger.loopAReq(count);
-            final long before = System.nanoTime();
-            loopAReq.call();
-            final long after = System.nanoTime();
-            final long duration = after - before;
-            Printer printer = new Printer(new IsolationReactor(facility1));
-            SpeedReport.startSReq(printer, "JVM Send Timings", duration, count).call();
+            Facility facility2 = new Facility();
+            try {
+                Ponger ponger = new Ponger(new NonBlockingReactor(facility1));
+                Pinger pinger = new Pinger(new NonBlockingReactor(facility2), ponger);
+                AsyncRequest<Void> loopAReq = pinger.loopAReq(count);
+                final long before = System.nanoTime();
+                loopAReq.call();
+                final long after = System.nanoTime();
+                final long duration = after - before;
+                Printer printer = new Printer(new IsolationReactor(facility1));
+                SpeedReport.startSReq(printer, "JVM Send Timings", duration, count).call();
+            } finally {
+                facility2.close();
+            }
         } finally {
             facility1.close();
-            facility2.close();
         }
     }
 }
