9136a44315d4e7d2750e8918c69607f06721bdc3
Bill La Forge <laforge49@gmail.com>
2013-10-07 15:49:53 +0530

    new SyncPrinterRequest

---
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/misc/SyncPrinterRequest.java
@@ -0,0 +1,42 @@
+package org.agilewiki.jactor2.core.blades.misc;
+
+import org.agilewiki.jactor2.core.messages.SyncRequest;
+import org.agilewiki.jactor2.core.reactors.Reactor;
+
+/**
+ * Base class for printer requests.
+ */
+abstract public class SyncPrinterRequest extends SyncRequest<Void> {
+
+    final protected Printer printer;
+
+    /**
+     * Create a SyncPrinterRequest.
+     *
+     * @param _printer    The blade this request is bound to.
+     */
+    public SyncPrinterRequest(final Printer _printer) {
+        super(_printer.getReactor());
+        printer = _printer;
+    }
+
+    /**
+     * Print a string.
+     *
+     * @param _string The string to be printed
+     */
+    protected void println(final String _string) throws Exception {
+        local(printer.printlnSReq(_string));
+    }
+
+    /**
+     * Print a formatted string.
+     *
+     * @param _format The formatting.
+     * @param _args   The data to be formatted.
+     */
+    protected void printf(final String _format,
+                          final Object... _args) throws Exception {
+        local(printer.printfSReq(_format, _args));
+    }
+}
