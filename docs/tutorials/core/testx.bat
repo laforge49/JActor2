b1a537163d124c552be4a9b6caf2dbc45b3fcd62
Bill La Forge <laforge49@gmail.com>
2013-10-07 16:52:51 +0530

    updated getting started

--- docs/tutorials/core/testx.bat
+++ docs/tutorials/core/testx.bat
@@ -1,3 +1,3 @@
 del *.class
-javac -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.3.4.jar;slf4j-api-1.7.5.jar *.java
-java -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.3.4.jar;slf4j-api-1.7.5.jar;slf4j-simple-1.7.5.jar;. %1
+javac -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.3.5.jar;slf4j-api-1.7.5.jar *.java
+java -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.3.5.jar;slf4j-api-1.7.5.jar;slf4j-simple-1.7.5.jar;. %1
