5df371d54f5cfce31cb6d243328070c327627c76
Bill La Forge <laforge49@gmail.com>
2013-10-18 11:09:49 +0530

    #40 dropped Event and EventBus from samples and tests

--- jactor2-core/src/samples/java/org/agilewiki/jactor2/core/messages/eventBus/StatusListener.java
+++
@@ -1,9 +0,0 @@
-package org.agilewiki.jactor2.core.messages.eventBus;
-
-import org.agilewiki.jactor2.core.blades.Blade;
-
-//An interface for blades which process StatusUpdate events.
-public interface StatusListener extends Blade {
-    //Process a StatusUpdate event.
-    void statusUpdate(final StatusUpdate _statusUpdate) throws Exception;
-}
