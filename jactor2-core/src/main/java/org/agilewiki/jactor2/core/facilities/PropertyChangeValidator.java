71db8cedc1c80eafbfa86a1e5fa363eef4ba5e91
Bill La Forge <laforge49@gmail.com>
2013-10-19 09:14:56 +0530

    #37 javadoc for PropertyChangeValidator

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/facilities/PropertyChangeValidator.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/facilities/PropertyChangeValidator.java
@@ -2,6 +2,17 @@ package org.agilewiki.jactor2.core.facilities;
 
 import org.agilewiki.jactor2.core.messages.AsyncRequest;
 
+/**
+ * Properties are validated before being applied.
+ */
 public interface PropertyChangeValidator {
-    AsyncRequest<String> validatePropertyChangeAReq(String _propertyName, Object oldValue, Object newValue);
+    /**
+     * The request returns a null if the property is valid, otherwise it returns an error message.
+     *
+     * @param _propertyName    The name of the property.
+     * @param _oldValue        The old value.
+     * @param _newValue        The proposed new value.
+     * @return Error message or null.
+     */
+    AsyncRequest<String> validatePropertyChangeAReq(String _propertyName, Object _oldValue, Object _newValue);
 }
