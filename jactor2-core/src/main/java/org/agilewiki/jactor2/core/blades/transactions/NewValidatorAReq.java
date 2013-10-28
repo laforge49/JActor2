46fa04bbc8293e032b1b2ff69fbbc8c4dbfd32fb
Bill La Forge <laforge49@gmail.com>
2013-10-28 17:57:40 +0530

    #51 NewPropertiesValidatorAReq

---
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/transactions/NewValidatorAReq.java
@@ -0,0 +1,43 @@
+package org.agilewiki.jactor2.core.blades.transactions;
+
+import org.agilewiki.jactor2.core.messages.AsyncRequest;
+import org.agilewiki.jactor2.core.messages.AsyncResponseProcessor;
+import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
+import org.agilewiki.jactor2.core.reactors.Reactor;
+
+abstract public class NewValidatorAReq<STATE, STATE_WRAPPER extends AutoCloseable, IMMUTABLE_CHANGES, IMMUTABLE_STATE>
+        extends AsyncRequest<ValidationSubscription<IMMUTABLE_CHANGES>> {
+    private AsyncRequest<ValidationSubscription<IMMUTABLE_CHANGES>> disReq = this;
+    private final TransactionProcessor<STATE, STATE_WRAPPER, IMMUTABLE_CHANGES, IMMUTABLE_STATE> transactionProcessor;
+
+    public NewValidatorAReq(
+            final NonBlockingReactor _targetReactor,
+            final TransactionProcessor<STATE, STATE_WRAPPER, IMMUTABLE_CHANGES, IMMUTABLE_STATE> _transactionProcessor) {
+        super(_targetReactor);
+        transactionProcessor = _transactionProcessor;
+    }
+
+    abstract protected String validateTransaction(IMMUTABLE_CHANGES _immutableChanges, AsyncResponseProcessor<String> rp)
+            throws Exception;
+
+    @Override
+    protected void processAsyncRequest() throws Exception {
+        Validator<IMMUTABLE_CHANGES> valTran = new Validator<IMMUTABLE_CHANGES>() {
+            @Override
+            public AsyncRequest<String> validateAReq(final IMMUTABLE_CHANGES _immutableChanges) {
+                return new AsyncRequest<String>(disReq.getTargetReactor()) {
+                    @Override
+                    protected void processAsyncRequest() throws Exception {
+                        validateTransaction(_immutableChanges, this);
+                    }
+                };
+            }
+
+            @Override
+            public Reactor getReactor() {
+                return disReq.getTargetReactor();
+            }
+        };
+        send(transactionProcessor.addValidatorAReq(valTran), this);
+    }
+}
---
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/transactions/properties/NewPropertiesValidatorAReq.java
@@ -0,0 +1,16 @@
+package org.agilewiki.jactor2.core.blades.transactions.properties;
+
+import org.agilewiki.jactor2.core.blades.transactions.NewValidatorAReq;
+import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
+
+import java.util.NavigableMap;
+import java.util.SortedMap;
+
+abstract public class NewPropertiesValidatorAReq extends NewValidatorAReq
+        <NavigableMap<String, Object>, PropertiesWrapper, PropertyChanges, SortedMap<String, Object>> {
+    public NewPropertiesValidatorAReq(
+            final NonBlockingReactor _targetReactor,
+            final PropertiesBlade _propertiesBlade) {
+        super(_targetReactor, _propertiesBlade.getPropertiesProcessor());
+    }
+}
