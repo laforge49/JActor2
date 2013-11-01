7523c1241f988696a6abf09d45445f754ca543e4
Bill La Forge <laforge49@gmail.com>
2013-11-01 12:03:35 +0530

    #59 PubSubSample

---
+++ jactor2-core/src/samples/java/org/agilewiki/jactor2/core/blades/pubSub/PubSubSample.java
@@ -0,0 +1,38 @@
+package org.agilewiki.jactor2.core.blades.pubSub;
+
+import org.agilewiki.jactor2.core.facilities.Plant;
+import org.agilewiki.jactor2.core.messages.AsyncResponseProcessor;
+import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
+
+public class PubSubSample {
+    public static void main(final String[] args) throws Exception {
+        final Plant plant = new Plant();
+        try {
+            NonBlockingReactor reactor = new NonBlockingReactor(plant);
+            RequestBus<String> requestBus =
+                    new RequestBus<String>(reactor);
+            new SubscribeAReq<String>(requestBus, reactor) {
+                @Override
+                protected void processNotification(String _content, AsyncResponseProcessor<Void> _asyncResponseProcessor) throws Exception {
+                    System.out.println("got " + _content);
+                    _asyncResponseProcessor.processAsyncResponse(null);
+                }
+            }.call();
+            new SubscribeAReq<String>(requestBus, reactor, new EqualsFilter<String>("ribit")) {
+                @Override
+                protected void processNotification(String _content, AsyncResponseProcessor<Void> _asyncResponseProcessor) throws Exception {
+                    System.out.println("*** Ribit! ***");
+                    _asyncResponseProcessor.processAsyncResponse(null);
+                }
+            }.call();
+            System.out.println("\nPublishing null.");
+            requestBus.sendsContentAReq(null).call();
+            System.out.println("\nPublishing ribit");
+            requestBus.sendsContentAReq("ribit").call();
+            System.out.println("\nPublishing abc");
+            requestBus.sendsContentAReq("abc").call();
+        } finally {
+            plant.close();
+        }
+    }
+}
