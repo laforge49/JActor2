e84a92fbc8a65662ded377f5fd5263ce12b0f222
Bill La Forge <laforge49@gmail.com>
2013-11-06 19:47:39 +0530

    #60 added missing put to PropertiesChangeManager

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/pubSub/transactions/properties/PropertiesChangeManager.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/pubSub/transactions/properties/PropertiesChangeManager.java
@@ -7,13 +7,31 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class PropertiesChangeManager implements AutoCloseable {
-    final ImmutableProperties<Object> immutableProperties;
+    ImmutableProperties<Object> immutableProperties;
     final TreeMap<String, PropertyChange> changes = new TreeMap<String, PropertyChange>();
     final public SortedMap<String, PropertyChange> readOnlyChanges = Collections.unmodifiableSortedMap(changes);
     private boolean closed;
 
     public PropertiesChangeManager(final ImmutableProperties<Object> _immutableProperties) {
         immutableProperties = _immutableProperties;
+    }
+
+    public void put(final String _key, final Object _newValue) {
+        if (closed) {
+            throw new IllegalStateException(
+                    "Already closed, the transaction is complete.");
+        }
+        if (_key == null)
+            throw new IllegalArgumentException("key may not be null");
+        Object oldValue = immutableProperties.get(_key);
+        if (oldValue == _newValue)
+            return;
+        if (_newValue == null)
+            immutableProperties = immutableProperties.minus(_key);
+        else
+            immutableProperties = immutableProperties.plus(_key, _newValue);
+        PropertyChange propertyChange = new PropertyChange(_key, oldValue, _newValue);
+        changes.put(_key, propertyChange);
     }
 
     @Override
