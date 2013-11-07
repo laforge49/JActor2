01a27624af5ea97ee388cf576c441f0e6c481e50
Bill La Forge <laforge49@gmail.com>
2013-11-07 10:50:08 +0530

    #60 PropertiesTransactionAReq

---
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/pubSub/transactions/properties/PropertiesTransactionAReq.java
@@ -0,0 +1,14 @@
+package org.agilewiki.jactor2.core.blades.pubSub.transactions.properties;
+
+import org.agilewiki.jactor2.core.blades.pubSub.transactions.TransactionAReq;
+import org.agilewiki.jactor2.core.blades.pubSub.transactions.TransactionProcessor;
+import org.agilewiki.jactor2.core.blades.pubSub.transactions.properties.immutable.ImmutableProperties;
+import org.agilewiki.jactor2.core.reactors.CommonReactor;
+
+public class PropertiesTransactionAReq
+        extends TransactionAReq<PropertiesChangeManager, ImmutableProperties<Object>, ImmutablePropertyChanges> {
+    public PropertiesTransactionAReq(final CommonReactor _updateReactor,
+                                     final PropertiesProcessor _transactionProcessor) {
+        super(_updateReactor, _transactionProcessor);
+    }
+}
