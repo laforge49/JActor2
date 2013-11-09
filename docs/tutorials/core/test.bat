50b66ea503ac494a174bdddb0df4220e1a51fd05
Bill La Forge <laforge49@gmail.com>
2013-11-09 13:02:25 +0530

    gettingStarted updated for pcollections

--- docs/tutorials/core/test.bat
+++ docs/tutorials/core/test.bat
@@ -1,3 +1,3 @@
 del *.class
 javac -classpath jactor2-core-0.5.0.jar;slf4j-api-1.7.5.jar *.java
-java -classpath jactor2-core-0.5.0.jar;slf4j-api-1.7.5.jar;slf4j-simple-1.7.5.jar;. %1
+java -classpath jactor2-core-0.5.0.jar;slf4j-api-1.7.5.jar;slf4j-simple-1.7.5.jar;pcollections-2.1.2.jar;. %1
--- docs/tutorials/core/testx.bat
+++ docs/tutorials/core/testx.bat
@@ -1,4 +1,4 @@
 del *.class
 javac -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.5.0.jar;slf4j-api-1.7.5.jar *.java
-java -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.5.0.jar;slf4j-api-1.7.5.jar;slf4j-simple-1.7.5.jar;. %1
+java -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.5.0.jar;slf4j-api-1.7.5.jar;slf4j-simple-1.7.5.jar;pcollections-2.1.2.jar;. %1
 del *.class
