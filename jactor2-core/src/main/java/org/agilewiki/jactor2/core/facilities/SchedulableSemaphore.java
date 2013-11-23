7dcab09b4f8b11b2b7f9924b24cb0820735c5334
Bill La Forge <laforge49@gmail.com>
2013-11-23 19:41:57 +0530

    #77 javadoc for SchedulableSemaphore

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/facilities/SchedulableSemaphore.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/facilities/SchedulableSemaphore.java
@@ -2,6 +2,9 @@ package org.agilewiki.jactor2.core.facilities;
 
 import java.util.concurrent.Semaphore;
 
+/**
+ * A timeout timer built on a semaphore, created by calling Plant.schedulableSemaphore(_millisecondDelay).
+ */
 public class SchedulableSemaphore {
     private Semaphore semaphore = new Semaphore(0);
     private boolean timeout;
@@ -17,11 +20,19 @@ public class SchedulableSemaphore {
     SchedulableSemaphore() {
     }
 
+    /**
+     * Wait for the timer to complete.
+     *
+     * @return True when the timeout expired, false when release was called.
+     */
     public boolean acquire() throws InterruptedException {
         semaphore.acquire();
         return timeout;
     }
 
+    /**
+     *  Force the timer to complete early.
+     */
     public void release() {
         semaphore.release();
     }
