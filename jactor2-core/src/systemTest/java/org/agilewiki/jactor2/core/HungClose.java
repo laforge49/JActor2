cf065b0eb21fb5ee78e2a573ea2ec52114fcda37
Bill La Forge <laforge49@gmail.com>
2013-12-08 07:11:13 +0530

    #78 system test Hangs -> HungClose

---
+++ jactor2-core/src/systemTest/java/org/agilewiki/jactor2/core/HungClose.java
@@ -0,0 +1,22 @@
+package org.agilewiki.jactor2.core;
+
+import org.agilewiki.jactor2.core.blades.BladeBase;
+import org.agilewiki.jactor2.core.facilities.Plant;
+import org.agilewiki.jactor2.core.messages.SyncRequest;
+import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
+
+public class HungClose {
+    static public void main(final String[] _args) throws Exception {
+        System.out.println("?");
+        final Plant plant = new Plant();
+        try {
+            NonBlockingReactor reactor = new NonBlockingReactor(plant);
+            Hanger hanger = new Hanger(reactor);
+            hanger.hangSReq().signal();
+        } finally {
+            System.out.println("closing");
+            plant.close();
+            System.out.println("closed");
+        }
+    }
+}
