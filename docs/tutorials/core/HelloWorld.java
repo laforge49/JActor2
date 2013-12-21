3c1b93e704e68bd933036dbc06db39681398664e
Bill La Forge <laforge49@gmail.com>
2013-12-21 11:51:29 +0530

    hello world page updated

--- docs/tutorials/core/HelloWorld.java
+++ docs/tutorials/core/HelloWorld.java
@@ -1,10 +1,11 @@
 import org.agilewiki.jactor2.core.blades.misc.Printer;
+import org.agilewiki.jactor2.core.plant.BasicPlant;
 import org.agilewiki.jactor2.core.plant.Plant;
 import org.agilewiki.jactor2.core.messages.SyncRequest;
 
 public class HelloWorld {
     public static void main(final String[] _args) throws Exception {
-        Plant plant = new Plant();
+        BasicPlant plant = new Plant();
         try {
             Printer.printlnAReq(plant, "Hello World!").call();
         } finally {
