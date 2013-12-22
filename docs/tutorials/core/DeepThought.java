2fb89bab41661722349d7243626aae37d6f42581
Bill La Forge <laforge49@gmail.com>
2013-12-22 21:42:30 +0530

    updated deep thought page

--- docs/tutorials/core/DeepThought.java
+++ docs/tutorials/core/DeepThought.java
@@ -1,14 +1,13 @@
 import org.agilewiki.jactor2.core.blades.NonBlockingBladeBase;
 import org.agilewiki.jactor2.core.blades.misc.Delay;
 import org.agilewiki.jactor2.core.blades.misc.Printer;
-import org.agilewiki.jactor2.core.facilities.Facility;
+import org.agilewiki.jactor2.core.plant.BasicPlant;
 import org.agilewiki.jactor2.core.plant.Plant;
 import org.agilewiki.jactor2.core.messages.AsyncRequest;
 import org.agilewiki.jactor2.core.messages.AsyncResponseProcessor;
 import org.agilewiki.jactor2.core.messages.SyncRequest;
 import org.agilewiki.jactor2.core.reactors.BlockingReactor;
 import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
-import org.agilewiki.jactor2.core.reactors.Reactor;
 
 public class DeepThought extends NonBlockingBladeBase {
     public DeepThought(final NonBlockingReactor _reactor) throws Exception {
@@ -32,16 +31,16 @@ public class DeepThought extends NonBlockingBladeBase {
                     new AsyncResponseProcessor<Void>() {
                 @Override
                 public void processAsyncResponse(final Void _response) throws Exception {
-                    AsyncRequest<Printer> stdoutRequest = Printer.stdoutAReq((Plant) getReactor().getFacility());
+                    AsyncRequest<Printer> stdoutRequest = Printer.stdoutAReq(getReactor().getPlant());
                     send(stdoutRequest, stdoutResponseProcessor);
                 }
             };
 
             @Override
             public void processAsyncRequest() throws Exception {
-                Reactor myReactor = getReactor();
-                Facility myFacility = myReactor.getFacility();
-                Delay delay = new Delay(new BlockingReactor(myFacility));
+                NonBlockingReactor myReactor = getReactor();
+                BasicPlant plant = myReactor.getPlant();
+                Delay delay = new Delay(new BlockingReactor(plant));
                 SyncRequest<Void> sleepSReq = delay.sleepSReq(4000);
                 send(sleepSReq, sleepResponseProcessor);
             }
@@ -49,7 +48,7 @@ public class DeepThought extends NonBlockingBladeBase {
     }
     
     public static void main(final String[] _args) throws Exception {
-        Plant plant = new Plant();
+        BasicPlant plant = new Plant();
         try {
             DeepThought deepThought = new DeepThought(new NonBlockingReactor(plant));
             AsyncRequest<Void> printAnswerAReq = deepThought.printAnswerAReq();
