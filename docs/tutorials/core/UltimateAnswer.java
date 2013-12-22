ff000b6f0bdcc09c0d2334cf461826913e03748f
Bill La Forge <laforge49@gmail.com>
2013-12-22 19:04:01 +0530

    ultimate answer page

--- docs/tutorials/core/UltimateAnswer.java
+++ docs/tutorials/core/UltimateAnswer.java
@@ -1,6 +1,6 @@
 import org.agilewiki.jactor2.core.blades.NonBlockingBladeBase;
 import org.agilewiki.jactor2.core.blades.misc.Printer;
-import org.agilewiki.jactor2.core.facilities.Facility;
+import org.agilewiki.jactor2.core.plant.BasicPlant;
 import org.agilewiki.jactor2.core.plant.Plant;
 import org.agilewiki.jactor2.core.messages.AsyncRequest;
 import org.agilewiki.jactor2.core.messages.AsyncResponseProcessor;
@@ -18,15 +18,15 @@ public class UltimateAnswer extends NonBlockingBladeBase {
 
             @Override
             public void processAsyncRequest() throws Exception {
-                Plant myPlant = getReactor().getPlant();
-                AsyncRequest<Void> printRequest = Printer.printlnAReq(myPlant, "*** 42 ***");
+                Plant plant = getReactor().getPlant();
+                AsyncRequest<Void> printRequest = Printer.printlnAReq(plant, "*** 42 ***");
                 send(printRequest, dis);
             }
         };
     }
     
     public static void main(final String[] _args) throws Exception {
-        Plant plant = new Plant();
+        BasicPlant plant = new Plant();
         try {
             UltimateAnswer ultimateAnswer = new UltimateAnswer(new NonBlockingReactor(plant));
             AsyncRequest<Void> printAnswerAReq = ultimateAnswer.printAnswerAReq();
