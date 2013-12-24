5d4ba8989f643cc84140203ff10b1a501e6a8b52
Bill La Forge <laforge49@gmail.com>
2013-12-24 10:05:09 +0530

    updated batcher page

--- docs/tutorials/core/Batcher.java
+++ docs/tutorials/core/Batcher.java
@@ -1,5 +1,5 @@
 import org.agilewiki.jactor2.core.blades.NonBlockingBladeBase;
-import org.agilewiki.jactor2.core.blades.misc.Printer;
+import org.agilewiki.jactor2.core.plant.BasicPlant;
 import org.agilewiki.jactor2.core.plant.Plant;
 import org.agilewiki.jactor2.core.messages.AsyncRequest;
 import org.agilewiki.jactor2.core.messages.AsyncResponseProcessor;
@@ -45,7 +45,7 @@ public class Batcher extends NonBlockingBladeBase {
     
     public static void main(final String[] _args) throws Exception {
         final long count = 1000000L;
-        Plant plant = new Plant();
+        BasicPlant plant = new Plant();
         try {
             Ponger ponger = new Ponger(new NonBlockingReactor(plant));
             Batcher batcher = new Batcher(new NonBlockingReactor(plant), count, ponger);
