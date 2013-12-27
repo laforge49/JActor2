a65adf0c7247e900955b28437102dc4f47494a53
Bill La Forge <laforge49@gmail.com>
2013-12-27 14:09:16 +0530

    #121 Closer now extends Blade, AutoCloseable

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/util/Closer.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/util/Closer.java
@@ -1,6 +1,8 @@
 package org.agilewiki.jactor2.core.util;
 
-public interface Closer {
+import org.agilewiki.jactor2.core.blades.Blade;
+
+public interface Closer extends Blade, AutoCloseable {
 
     boolean addCloseable(final Closeable _closeable) throws Exception;
     boolean removeCloseable(final Closeable _closeable);
