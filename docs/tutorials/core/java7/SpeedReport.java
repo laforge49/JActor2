df8956af9b084a04880f7766d1e801538ff2b3eb
Bill La Forge <laforge49@gmail.com>
2014-01-02 12:01:03 +0530

    #126 updated call speed report page

--- docs/tutorials/core/java7/SpeedReport.java
+++ docs/tutorials/core/java7/SpeedReport.java
@@ -7,7 +7,9 @@ public class SpeedReport {
         System.out.println(_heading);
         System.out.println("Test duration in nanoseconds: " + _ns);
         System.out.println("Number of exchanges: " + _count);
-        if (_ns > 0)
+        if (_ns > 0) {
             System.out.println("Exchanges per second: " + (1000000000L * _count / _ns));
+            System.out.println("Latency in seconds: " + (_ns / (1000000000.0 * _count)));
+        }
     }
 }
