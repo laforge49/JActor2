c8d33b9aa67445da34a7c8d10a2ad450497b6e1f
Bill La Forge <laforge49@gmail.com>
2014-01-22 18:27:51 +0530

    #133 new SleeperMessageTimeout example

---
+++ docs/tutorials/core/java7/Hanger.java
@@ -0,0 +1,32 @@
+import org.agilewiki.jactor2.core.blades.NonBlockingBladeBase;
+import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
+import org.agilewiki.jactor2.core.requests.SyncRequest;
+
+class Hanger extends NonBlockingBladeBase {
+    Hanger() throws Exception {
+        super(new NonBlockingReactor());
+    }
+
+    SyncRequest<Void> looperSReq() {
+        return new SyncBladeRequest<Void>() {
+            @Override
+            public Void processSyncRequest() throws Exception {
+                while (true) {}
+            }
+        };
+    }
+
+    SyncRequest<Void> sleeperSReq() {
+        return new SyncBladeRequest<Void>() {
+            @Override
+            public Void processSyncRequest() throws Exception {
+                try {
+                    Thread.sleep(Long.MAX_VALUE);
+                } catch (InterruptedException ie) {
+                    throw ie;
+                }
+                return null;
+            }
+        };
+    }
+}
