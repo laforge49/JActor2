a7ca9cc374779ba408262101472977c86fe4ea58
Bill La Forge <laforge49@gmail.com>
2014-01-23 17:57:10 +0530

    #121 javadocs for Scheduler

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/plant/Scheduler.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/plant/Scheduler.java
@@ -1,8 +1,34 @@
 package org.agilewiki.jactor2.core.plant;
 
+/**
+ * A schedular for Plant.
+ */
 public interface Scheduler {
-    void schedule(Runnable runnable, long _millisecondDelay);
-    void scheduleAtFixedRate(Runnable runnable, long _millisecondDelay);
+    /**
+     * Schedule a Runnable to be run at a later time.
+     *
+     * @param _runnable          The Runnable to be scheduled.
+     * @param _millisecondDelay How long to wait before the Runnable is to be run.
+     */
+    void schedule(Runnable _runnable, long _millisecondDelay);
+
+    /**
+     * Schedule a Runnable to be run repeatedly.
+     *
+     * @param _runnable          The Runnable to be run.
+     * @param _millisecondDelay The delay between each run.
+     */
+    void scheduleAtFixedRate(Runnable _runnable, long _millisecondDelay);
+
+    /**
+     * Returns the approximate time.
+     *
+     * @return The approximate time.
+     */
     long currentTimeMillis();
+
+    /**
+     * Shut down the scheduler thread pool.
+     */
     void close();
 }
