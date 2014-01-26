2eaabff51ce3626297cfaeb1c1b046a9642b38d5
Bill La Forge <laforge49@gmail.com>
2014-01-26 12:11:28 +0530

    RequestImpl.getMessageSource -> getRequestSource

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/impl/RequestImplBase.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/impl/RequestImplBase.java
@@ -103,7 +103,7 @@ public abstract class RequestImplBase<RESPONSE_TYPE> implements RequestImpl<RESP
     public Reactor getTargetReactor() { return targetReactor; }
 
     @Override
-    public MessageSource getMessageSource() {
+    public MessageSource getRequestSource() {
         return messageSource;
     }
 
--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/plant/package-info.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/plant/package-info.java
@@ -4,5 +4,17 @@
  * Plant takes its name from <i>manufacturing plant</i>, with the reactors performing the application processing.
  * A plant consists of a configuration, a recovery strategy, a scheduler, a thread pool and an internal reactor.
  * </p>
+ * <p>
+ * Relevant tutorial pages:
+ * </p>
+ * <ul>
+ * <li>
+ * <a href="../../../../../../tutorials/core/gettingStarted.html" target="_top">Getting Started</a>
+ * -- See system properties Debug and Configuration
+ * </li>
+ * <li>
+ * <a href="../../../../../../tutorials/core/recovery.html" target="_top">Recovery</a>
+ * </li>
+ * </ul>
  */
 package org.agilewiki.jactor2.core.plant;
