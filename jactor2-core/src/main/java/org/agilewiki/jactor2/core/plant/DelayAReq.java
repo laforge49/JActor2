eaf85b9a84021f2d34b0cdddaffeea945988100a
Bill La Forge <laforge49@gmail.com>
2014-01-27 15:24:04 +0530

    #134 new DelayAReq

---
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/plant/DelayAReq.java
@@ -0,0 +1,63 @@
+package org.agilewiki.jactor2.core.plant;
+
+import org.agilewiki.jactor2.core.closeable.FutureCloser;
+import org.agilewiki.jactor2.core.reactors.Reactor;
+import org.agilewiki.jactor2.core.requests.AsyncRequest;
+
+import java.util.concurrent.ScheduledFuture;
+
+/**
+ * Returns a result after a delay.
+ */
+public class DelayAReq extends AsyncRequest<Void> {
+    private final long millisecondDelay;
+    private FutureCloser futureCloser;
+    private ScheduledFuture<?> scheduledFuture;
+    private boolean canceled;
+
+    public DelayAReq(long _millisecondDelay) {
+        super(Plant.getInternalReactor());
+        millisecondDelay = _millisecondDelay;
+    }
+
+    /**
+     * Cancel the response if not yet sent.
+     */
+    public void cancel() throws Exception {
+        canceled = true;
+        if (futureCloser != null)
+            futureCloser.close();
+        else
+            scheduledFuture.cancel(false);
+    }
+
+    /**
+     * Returns true if the response was canceled.
+     *
+     * @return True if the response was canceled.
+     */
+    public boolean isCanceled() {
+        return canceled;
+    }
+
+    @Override
+    public void processAsyncRequest() throws Exception {
+        PlantScheduler plantScheduler = Plant.getPlantScheduler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (futureCloser != null)
+                        futureCloser.close();
+                    processAsyncResponse(null);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        scheduledFuture = plantScheduler.schedule(runnable, millisecondDelay);
+        Reactor sourceReactor = getSourceReactor();
+        if (sourceReactor != null)
+            futureCloser = new FutureCloser(scheduledFuture);
+    }
+}
