db4523260861a910261a12c27897f840fd0e7183
Bill La Forge <laforge49@gmail.com>
2014-02-03 16:33:27 +0530

    #121 javadocs for NonBlockingReactor

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/reactors/NonBlockingReactor.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/reactors/NonBlockingReactor.java
@@ -5,36 +5,31 @@ import org.agilewiki.jactor2.core.impl.PlantImpl;
 import org.agilewiki.jactor2.core.plant.Plant;
 
 /**
- * A targetReactor for blades which process messages quickly and without blocking the thread.
+ * Process requests/responses quickly and without blocking the thread.
  * <p>
- * For thread safety, the processing of each message is done in isolation, but when the processing of a
- * message results in the sending of a request, other messages may be processed before a
- * response to that request is received.
+ * Requests/responses are processed one at a time in the order received, except that
+ * requests/responses from the same reactor are given preference.
  * </p>
  * <p>
- * AsyncRequest/Response messages which are destined to a different targetReactor are buffered rather
- * than being sent immediately. These messages are disbursed to their destinations when all
+ * Requests/responses destined to a different reactor are held until all
  * incoming messages have been processed.
- * </p>
- * <p>
- * When the last block of buffered messages is being disbursed, if the destination is not
- * a thread-bound targetReactor, the destination targetReactor has no associated thread and the
- * facility of the current targetReactor is the same as the destination targetReactor, then the
- * current thread migrates with the message block. By this means the message block is
- * often kept in the hardware thread's high-speed memory cache, which means much faster
- * execution.
- * </p>
- * <p>
- * The Inbox used by NonBlockingReactor is NonBlockingInbox.
  * </p>
  */
 public class NonBlockingReactor extends ReactorBase implements CommonReactor {
 
+    /**
+     * Create a reactor with the Plant internal reactor as the parent.
+     */
     public NonBlockingReactor()
             throws Exception {
         this(Plant.getInternalReactor());
     }
 
+    /**
+     * Create a reactor.
+     *
+     * @param _parentReactor            The parent reactor.
+     */
     public NonBlockingReactor(final NonBlockingReactor _parentReactor)
             throws Exception {
         this(_parentReactor,
@@ -50,16 +45,37 @@ public class NonBlockingReactor extends ReactorBase implements CommonReactor {
                                 PlantImpl.getSingleton().getPlantConfiguration().getInitialLocalMessageQueueSize());
     }
 
+    /**
+     * Create a reactor with the Plant internal reactor as the parent.
+     *
+     * @param _initialOutboxSize        Initial size of the list of requests/responses for each destination.
+     * @param _initialLocalQueueSize    Initial size of the local input queue.
+     */
     public NonBlockingReactor(final int _initialOutboxSize, final int _initialLocalQueueSize) throws Exception {
         this(Plant.getInternalReactor(), _initialOutboxSize, _initialLocalQueueSize);
     }
 
+    /**
+     * Create a reactor.
+     *
+     * @param _parentReactor            The parent reactor.
+     * @param _initialOutboxSize        Initial size of the list of requests/responses for each destination.
+     * @param _initialLocalQueueSize    Initial size of the local input queue.
+     */
     public NonBlockingReactor(final NonBlockingReactor _parentReactor,
                               final int _initialOutboxSize, final int _initialLocalQueueSize) throws Exception {
         initialize(createReactorImpl(_parentReactor == null ? null : _parentReactor.asReactorImpl(),
                 _initialOutboxSize, _initialLocalQueueSize));
     }
 
+    /**
+     * Create the object used to implement the reactor.
+     *
+     * @param _parentReactorImpl        The parent reactor impl object.
+     * @param _initialOutboxSize        Initial size of the list of requests/responses for each destination.
+     * @param _initialLocalQueueSize    Initial size of the local input queue.
+     * @return The object used to implement the reactor.
+     */
     protected NonBlockingReactorImpl createReactorImpl(final NonBlockingReactorImpl _parentReactorImpl,
                                                        final int _initialOutboxSize, final int _initialLocalQueueSize)
             throws Exception {
@@ -71,6 +87,11 @@ public class NonBlockingReactor extends ReactorBase implements CommonReactor {
         return (NonBlockingReactorImpl) super.asReactorImpl();
     }
 
+    /**
+     * Define the activity which occurs when the input queue is empty.
+     *
+     * @param _idle    The activity which occurs when the input queue is empty.
+     */
     public void setIdle(final Runnable _idle) {
         ((NonBlockingReactorImpl) asReactorImpl()).onIdle = _idle;
     }
