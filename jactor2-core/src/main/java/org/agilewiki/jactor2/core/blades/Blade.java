2fb71b44bd1013323c900d76c8e53f7315367405
Bill La Forge <laforge49@gmail.com>
2014-02-05 23:21:11 +0530

    #121 javadocs for Blade

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/Blade.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/Blade.java
@@ -4,14 +4,13 @@ import org.agilewiki.jactor2.core.reactors.Reactor;
 
 /**
  * <p>
- * A blade is an object with persistent state and a Reactor and, typically, requests.
- * Thread safety is achieved by restricting its access to the requests of the blade
- * and to the requests of other blades which share the same reactor.
+ * A blade is an object with persistent state operated on by requests
+ * within the thread context of a single reactor.
  * </p>
  */
 public interface Blade {
     /**
-     * Returns the reactor used by this blade.
+     * Returns the reactor used by the requests of this blade.
      *
      * @return The Reactor.
      */
