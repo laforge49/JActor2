515a1a18e7540f0a9a37efb506e01bbe3e4cd820
Bill La Forge <laforge49@gmail.com>
2014-02-09 19:14:38 +0530

    #146 demo the cancel bug--ICloseTest (disabled)

---
+++ jactor2-core/src/test/java/org/agilewiki/jactor2/core/reactors/ICloseTest.java
@@ -0,0 +1,63 @@
+package org.agilewiki.jactor2.core.reactors;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor2.core.blades.BlockingBladeBase;
+import org.agilewiki.jactor2.core.blades.IsolationBladeBase;
+import org.agilewiki.jactor2.core.plant.DelayAReq;
+import org.agilewiki.jactor2.core.plant.Plant;
+import org.agilewiki.jactor2.core.requests.AsyncRequest;
+import org.agilewiki.jactor2.core.requests.AsyncResponseProcessor;
+
+public class ICloseTest extends TestCase {
+    public void testa() throws Exception {
+        new Plant();
+        try {
+            new IHang().goAReq().call();
+        } finally {
+            Plant.close();
+        }
+    }
+}
+
+class IHang extends BlockingBladeBase {
+
+    IHang() throws Exception {
+    }
+
+    AsyncRequest<Void> goAReq() {
+        return new AsyncBladeRequest<Void>() {
+            AsyncRequest<Void> dis = this;
+            IHung iHung;
+
+            @Override
+            public void processAsyncRequest() throws Exception {
+                iHung = new IHung();
+                final AsyncRequest<Void> noRspAReq = iHung.noRspAReq();
+                //send(noRspAReq, dis);
+                send(iHung.getReactor().nullSReq(), dis);
+                send(new DelayAReq(50), new AsyncResponseProcessor<Void>() {
+                    @Override
+                    public void processAsyncResponse(Void _response) throws Exception {
+                        cancel(noRspAReq);
+                    }
+                });
+            }
+        };
+    }
+}
+
+class IHung extends IsolationBladeBase {
+
+    IHung() throws Exception {
+    }
+
+    AsyncRequest<Void> noRspAReq() {
+        return new AsyncBladeRequest<Void>() {
+            @Override
+            public void processAsyncRequest() throws Exception {
+                setNoHungRequestCheck();
+                System.out.println("hi");
+            }
+        };
+    }
+}
