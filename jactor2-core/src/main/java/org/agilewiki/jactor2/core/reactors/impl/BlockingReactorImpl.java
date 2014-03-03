2d5752ff4d05c334ea52183d2b8a945f9b1f36f6
Bill La Forge <laforge49@gmail.com>
2014-03-03 17:14:33 +0530

    #139 updated BlockingReactorImpl

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/reactors/impl/BlockingReactorImpl.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/reactors/impl/BlockingReactorImpl.java
@@ -3,8 +3,18 @@ package org.agilewiki.jactor2.core.reactors.impl;
 import org.agilewiki.jactor2.core.reactors.BlockingReactor;
 import org.agilewiki.jactor2.core.requests.impl.RequestImpl;
 
+/**
+ * The internal implementation of BlockingReactor.
+ */
 public class BlockingReactorImpl extends UnboundReactorImpl {
 
+    /**
+     * Create a BlockingReactorImpl.
+     *
+     * @param _parentReactorImpl        The parent reactor.
+     * @param _initialOutboxSize        The initial buffer size for outgoing messages.
+     * @param _initialLocalQueueSize    The initial local queue size.
+     */
     public BlockingReactorImpl(final NonBlockingReactorImpl _parentReactorImpl,
                                final int _initialOutboxSize, final int _initialLocalQueueSize) {
         super(_parentReactorImpl, _initialOutboxSize, _initialLocalQueueSize);
