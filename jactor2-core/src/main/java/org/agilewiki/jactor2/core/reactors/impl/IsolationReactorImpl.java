40fad3de0180de5f51e34fca0ce1113f29b6e9b3
Bill La Forge <laforge49@gmail.com>
2014-03-03 17:17:10 +0530

    #139 updated IsolationReactorImpl javadocs

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/reactors/impl/IsolationReactorImpl.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/reactors/impl/IsolationReactorImpl.java
@@ -3,8 +3,18 @@ package org.agilewiki.jactor2.core.reactors.impl;
 import org.agilewiki.jactor2.core.requests.impl.RequestImpl;
 import org.agilewiki.jactor2.core.reactors.IsolationReactor;
 
+/**
+ * Internal implementation of UnboundReactor.
+ */
 public class IsolationReactorImpl extends UnboundReactorImpl {
 
+    /**
+     * Create an IsolationReactorImpl.
+     *
+     * @param _parentReactorImpl        The parent reactor.
+     * @param _initialOutboxSize        The initial buffer size for outgoing messages.
+     * @param _initialLocalQueueSize    The initial local queue size.
+     */
     public IsolationReactorImpl(final NonBlockingReactorImpl _parentReactorImpl,
                                 final int _initialOutboxSize, final int _initialLocalQueueSize) {
         super(_parentReactorImpl, _initialOutboxSize, _initialLocalQueueSize);
