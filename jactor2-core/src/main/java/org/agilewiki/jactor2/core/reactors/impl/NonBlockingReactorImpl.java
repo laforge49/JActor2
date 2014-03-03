167e61d77c660710edcb838803d92d59d97f87d0
Bill La Forge <laforge49@gmail.com>
2014-03-03 17:21:45 +0530

    #139 updated NonBlockingReactorImpl javadocs

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/reactors/impl/NonBlockingReactorImpl.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/reactors/impl/NonBlockingReactorImpl.java
@@ -2,8 +2,18 @@ package org.agilewiki.jactor2.core.reactors.impl;
 
 import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
 
+/**
+ * Internal implementation of NonBlockingReactor.
+ */
 public class NonBlockingReactorImpl extends UnboundReactorImpl {
 
+    /**
+     * Create a NonBlockingReactorImpl.
+     *
+     * @param _parentReactorImpl        The parent reactor.
+     * @param _initialOutboxSize        The initial buffer size for outgoing messages.
+     * @param _initialLocalQueueSize    The initial local queue size.
+     */
     public NonBlockingReactorImpl(final NonBlockingReactorImpl _parentReactorImpl,
                                   final int _initialOutboxSize, final int _initialLocalQueueSize) {
         super(_parentReactorImpl, _initialOutboxSize, _initialLocalQueueSize);
