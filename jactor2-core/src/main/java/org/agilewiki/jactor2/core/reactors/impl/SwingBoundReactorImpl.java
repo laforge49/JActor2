d6863f9cf9416294e7b44355ec34c62dc3553e0c
Bill La Forge <laforge49@gmail.com>
2014-03-03 20:23:39 +0530

    #139 updated SwingBoundReactorImpl javadocs

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/reactors/impl/SwingBoundReactorImpl.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/reactors/impl/SwingBoundReactorImpl.java
@@ -4,12 +4,27 @@ import org.agilewiki.jactor2.core.reactors.SwingBoundReactor;
 
 import javax.swing.*;
 
+/**
+ * A reactor bound to the Swing UI thread.
+ */
 public class SwingBoundReactorImpl extends ThreadBoundReactorImpl {
 
+    /**
+     * Returns the SwingBoundReactorImpl bound to the current thread.
+     *
+     * @return A SwingBoundReactorImpl, or null.
+     */
     public static SwingBoundReactorImpl threadReactor() {
         return (SwingBoundReactorImpl) ThreadBoundReactorImpl.threadReactor();
     }
 
+    /**
+     * Create a SwingBoundReactorImpl.
+     *
+     * @param _parentReactorImpl        The parent reactor.
+     * @param _initialOutboxSize        The initial buffer size for outgoing messages.
+     * @param _initialLocalQueueSize    The initial local queue size.
+     */
     public SwingBoundReactorImpl(final NonBlockingReactorImpl _parentReactorImpl,
                                  final int _initialOutboxSize, final int _initialLocalQueueSize) {
         super(_parentReactorImpl, _initialOutboxSize, _initialLocalQueueSize, null);
