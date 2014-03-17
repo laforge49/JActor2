5b62e0c46b3e7c3289d52b4bd436f4af4757a92d
Bill La Forge <laforge49@gmail.com>
2014-03-17 16:14:52 +0530

    #172 moved modules code to its own project

--- jactor2-modules/src/main/java/org/agilewiki/jactor2/modules/DependencyNotPresentException.java
+++
@@ -1,7 +0,0 @@
-package org.agilewiki.jactor2.modules;
-
-public class DependencyNotPresentException extends Exception {
-    public DependencyNotPresentException(final String _name) {
-        super(_name);
-    }
-}
--- jactor2-modules/src/main/java/org/agilewiki/jactor2/modules/FacilityAlreadyPresentException.java
+++
@@ -1,7 +0,0 @@
-package org.agilewiki.jactor2.modules;
-
-public class FacilityAlreadyPresentException extends Exception {
-    public FacilityAlreadyPresentException(final String _name) {
-        super(_name);
-    }
-}
