cf19e93064e8c339fa42312d1c0e236dbb2754d7
Bill La Forge <laforge49@gmail.com>
2014-03-17 13:58:54 +0530

    #172 moved util to its own project

--- jactor2-util/src/main/java/org/agilewiki/jactor2/util/durable/incDes/JAString.java
+++
@@ -1,6 +0,0 @@
-package org.agilewiki.jactor2.util.durable.incDes;
-
-public interface JAString extends VLenS<String> {
-
-    public static final String FACTORY_NAME = "string";
-}
--- jactor2-util/src/main/java/org/agilewiki/jactor2/util/durable/incDes/Tuple.java
+++
@@ -1,9 +0,0 @@
-package org.agilewiki.jactor2.util.durable.incDes;
-
-import org.agilewiki.jactor2.util.durable.JASerializable;
-
-/**
- * An ordered set of different types of serializable objects.
- */
-public interface Tuple extends Collection<JASerializable> {
-}
