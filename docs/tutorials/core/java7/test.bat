0707ce39cb9d9af4361fe660af8e45b99998937e
Bill La Forge <laforge49@gmail.com>
2014-03-22 13:50:01 +0530

    updated getting started for 0.8.2

--- docs/tutorials/core/java7/test.bat
+++ docs/tutorials/core/java7/test.bat
@@ -1,3 +1,3 @@
 del *.class
-javac -classpath jactor2-core-0.8.1.jar;slf4j-api-1.7.5.jar *.java
-java -classpath jactor2-core-0.8.1.jar;slf4j-api-1.7.5.jar;slf4j-simple-1.7.5.jar;guava-15.0.jar;. %1
+javac -classpath jactor2-core-0.8.2.jar;slf4j-api-1.7.5.jar *.java
+java -classpath jactor2-core-0.8.2.jar;slf4j-api-1.7.5.jar;slf4j-simple-1.7.5.jar;guava-15.0.jar;. %1
--- docs/tutorials/core/java7/testy.bat
+++ docs/tutorials/core/java7/testy.bat
@@ -1,4 +1,4 @@
 del *.class
-javac -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.8.1.jar;slf4j-api-1.7.5.jar -Xlint:deprecation -Xlint:unchecked *.java
-java -Djactor.debug=true -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.8.1.jar;slf4j-api-1.7.5.jar;slf4j-simple-1.7.5.jar;guava-15.0.jar;. %1
+javac -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.8.2.jar;slf4j-api-1.7.5.jar -Xlint:deprecation -Xlint:unchecked *.java
+java -Djactor.debug=true -classpath C:\GitHub\JActor2\jactor2-core\target\jactor2-core-0.8.2.jar;slf4j-api-1.7.5.jar;slf4j-simple-1.7.5.jar;guava-15.0.jar;. %1
 del *.class
