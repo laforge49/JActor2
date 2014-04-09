d2bf105948d51e1eecc40c8557599e2794c5f374
Bill La Forge <laforge49@gmail.com>
2014-04-09 11:04:30 +0530

    #178 moved tests, samples and system tests to core impl project

---
+++ jactor2-coreImpl/src/main/java/org/agilewiki/jactor2/core/impl/XPlant.java
@@ -0,0 +1,41 @@
+package org.agilewiki.jactor2.core.impl;
+
+import org.agilewiki.jactor2.core.impl.plantImpl.PlantImpl;
+import org.agilewiki.jactor2.core.plant.PlantBase;
+import org.agilewiki.jactor2.core.plant.PlantConfiguration;
+import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
+
+/**
+ * Plant is a singleton and is the top-level object.
+ * The plant has an internal reactor which is the root of a tree of all reactors,
+ * though this tree is made using weak references.
+ */
+final public class XPlant extends PlantBase {
+
+    /**
+     * Create a plant with the default configuration.
+     */
+    public XPlant() {
+        new PlantImpl();
+    }
+
+    /**
+     * Create a plant with the default configuration,
+     * but with the given reactor thread pool size.
+     *
+     * @param _reactorThreadPoolSize    The number of threads to be created for the
+     *                                  reactor thread pool.
+     */
+    public XPlant(final int _reactorThreadPoolSize) {
+        new PlantImpl(_reactorThreadPoolSize);
+    }
+
+    /**
+     * Create a plant with the given configuration.
+     *
+     * @param _plantConfiguration    The configuration to be used by the plant.
+     */
+    public XPlant(final PlantConfiguration _plantConfiguration) {
+        new PlantImpl(_plantConfiguration);
+    }
+}
