7f6a1998e34713030a2f795c26ce33161c960cea
skunkiferous <s.diot@eurodata.de>
2014-04-12 17:00:49 +0200

    Added GWT configuration for upcoming GWT-compatible JActor2 single-threaded version

---
+++ jactor2-coreSt/src/main/java/org/agilewiki/jactor2/core/stimpl/EntryPoint.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2014 Sebastien Diot.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.agilewiki.jactor2.core.stimpl;
+
+/**
+ * JActor2-Core-St EntryPoint.
+ *
+ * Basically does nothing, except make sure JActor2-Core is compilable with GWT.
+ *
+ * @author monster
+ */
+public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+	// NOP
+    }
+}
