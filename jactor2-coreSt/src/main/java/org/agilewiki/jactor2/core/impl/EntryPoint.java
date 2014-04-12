e25d9053f1122ff5fde09eca0281c11b91219928
skunkiferous <s.diot@eurodata.de>
2014-04-12 17:53:45 +0200

    Renamed package

--- jactor2-coreSt/src/main/java/org/agilewiki/jactor2/core/impl/EntryPoint.java
+++ jactor2-coreSt/src/main/java/org/agilewiki/jactor2/core/impl/EntryPoint.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.agilewiki.jactor2.core.stimpl;
+package org.agilewiki.jactor2.core.impl;
 
 /**
  * JActor2-Core-St EntryPoint.
