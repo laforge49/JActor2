cd9fea4bc1af0080d846525b246b6b26f322aa27
Bill La Forge <laforge49@gmail.com>
2014-04-13 09:52:24 +0530

    #178 reworked DefaultPlantScheduler in coreSt

--- jactor2-coreSt/src/main/java/org/agilewiki/jactor2/core/impl/stPlant/DefaultPlantScheduler.java
+++ jactor2-coreSt/src/main/java/org/agilewiki/jactor2/core/impl/stPlant/DefaultPlantScheduler.java
@@ -1,48 +1,64 @@
 package org.agilewiki.jactor2.core.impl.stPlant;
 
-//import java.util.concurrent.ScheduledFuture;
+import org.agilewiki.jactor2.core.plant.PlantScheduler;
+
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A scheduler for Plant, created by PlantConfiguration.
  */
-public class DefaultPlantScheduler {
-    /**
-     * Schedule a Runnable to be run at a later time.
-     *
-     * @param _runnable          The Runnable to be scheduled.
-     * @param _millisecondDelay How long to wait before the Runnable is to be run.
-     */
-    /*
-    public ScheduledFuture<?> schedule(Runnable _runnable, long _millisecondDelay) {
+public class DefaultPlantScheduler implements PlantScheduler {
 
-    }
-    */
+    private volatile long currentTimeMillis;
+
+    private ScheduledThreadPoolExecutor scheduledThreadPoolExecutor;
 
     /**
-     * Schedule a Runnable to be run repeatedly.
-     *
-     * @param _runnable          The Runnable to be run.
-     * @param _millisecondDelay The delay between each run.
+     * Creates the default plantScheduler.
      */
-    /*
-    public ScheduledFuture<?> scheduleAtFixedRate(Runnable _runnable, long _millisecondDelay) {
-
-    }
-    */
-
-    /**
-     * Returns the approximate time.
-     *
-     * @return The approximate time.
-     */
-    public long currentTimeMillis() {
-        return 0;
+    public DefaultPlantScheduler() {
+        scheduledThreadPoolExecutor =
+                new ScheduledThreadPoolExecutor(getSchedulerPoolSize());
+        currentTimeMillis = System.currentTimeMillis();
+        scheduledThreadPoolExecutor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                currentTimeMillis = System.currentTimeMillis();
+            }
+        }, getHeartbeatMillis(), getHeartbeatMillis(), TimeUnit.MILLISECONDS);
     }
 
     /**
-     * Shut down the plantScheduler thread pool.
+     * Controls how often currentTimeMillis is updated: every 500 milliseconds.
+     * @return The number of milliseconds between updates to currentTimeMillis.
      */
-    void close() {
+    protected long getHeartbeatMillis() { return 500; }
 
+    /**
+     * Determines the size of the scheduledThreadPool: 2.
+     *
+     * @return Returns the number of threads in the scheduledThreadPool.
+     */
+    protected int getSchedulerPoolSize() { return 2; }
+
+    @Override
+    public long currentTimeMillis() { return currentTimeMillis; }
+
+    @Override
+    public ScheduledFuture<?> schedule(Runnable runnable, long _millisecondDelay) {
+        return scheduledThreadPoolExecutor.schedule(runnable, _millisecondDelay, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleAtFixedRate(Runnable runnable, long _millisecondDelay) {
+        return scheduledThreadPoolExecutor.scheduleAtFixedRate(runnable, _millisecondDelay,
+                _millisecondDelay, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void close() {
+        scheduledThreadPoolExecutor.shutdown();
     }
 }
