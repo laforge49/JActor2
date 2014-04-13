d6480aac187b794bd06e08ea328506fd22152aeb
Bill La Forge <laforge49@gmail.com>
2014-04-13 10:44:21 +0530

    #178 first draft of PlantStImpl

---
+++ jactor2-coreSt/src/main/java/org/agilewiki/jactor2/core/impl/stPlant/PlantStImpl.java
@@ -0,0 +1,212 @@
+package org.agilewiki.jactor2.core.impl.stPlant;
+
+import org.agilewiki.jactor2.core.plant.PlantImpl;
+import org.agilewiki.jactor2.core.plant.PlantScheduler;
+import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
+import org.agilewiki.jactor2.core.reactors.PoolThreadReactorImpl;
+import org.agilewiki.jactor2.core.reactors.Reactor;
+import org.agilewiki.jactor2.core.reactors.ReactorImpl;
+import org.agilewiki.jactor2.core.requests.AsyncRequest;
+import org.agilewiki.jactor2.core.requests.AsyncRequestImpl;
+import org.agilewiki.jactor2.core.requests.RequestImpl;
+import org.agilewiki.jactor2.core.requests.SyncRequest;
+
+public class PlantStImpl extends PlantImpl {
+
+    /**
+     * Returns this singleton.
+     *
+     * @return This singleton.
+     */
+    public static PlantStImpl getSingleton() {
+        return (PlantStImpl) PlantImpl.getSingleton();
+    }
+
+    /**
+     * System property flag, jactor.debug, to turn on debug;
+     */
+    public static final boolean DEBUG = "true".equals(System
+            .getProperty("jactor.debug"));
+
+    private PlantConfiguration plantConfiguration;
+
+    private NonBlockingReactor internalReactor;
+
+    public ReactorImpl currentReactorImpl;
+
+    /**
+     * Create the singleton with a default configuration.
+     */
+    public PlantStImpl() {
+        this(new PlantConfiguration());
+    }
+
+    /**
+     * Create the singleton with the given configuration.
+     *
+     * @param _plantConfiguration The configuration to be used by the singleton.
+     */
+    public PlantStImpl(final PlantConfiguration _plantConfiguration) {
+        if (DEBUG) {
+            System.out.println("\n*** jactor.debug = true ***\n");
+        }
+        String configurationClassName = System.getProperty("jactor.configurationClass");
+        if (configurationClassName != null) {
+            ClassLoader classLoader = getClass().getClassLoader();
+            Class configurationClass = null;
+            try {
+                configurationClass = classLoader.loadClass(configurationClassName);
+            } catch (ClassNotFoundException e) {
+                throw new IllegalArgumentException("unable to load class " + configurationClassName, e);
+            }
+            try {
+                plantConfiguration = (PlantConfiguration) configurationClass.newInstance();
+            } catch (InstantiationException e) {
+                throw new IllegalArgumentException("unable to instantiate " + configurationClassName, e);
+            } catch (IllegalAccessException e) {
+                throw new IllegalArgumentException("unable to instantiate " + configurationClassName, e);
+            }
+        } else
+            plantConfiguration = _plantConfiguration;
+        long reactorPollMillis = _plantConfiguration.getRecovery().getReactorPollMillis();
+        internalReactor = createInternalReactor();
+        _plantConfiguration.getPlantScheduler().scheduleAtFixedRate(plantPoll(),
+                reactorPollMillis);
+    }
+
+    @Override
+    public ReactorImpl getCurrentReactorImpl() {
+        return currentReactorImpl;
+    }
+
+    @Override
+    public ReactorImpl createNonBlockingReactorImpl(final NonBlockingReactor _parentReactor,
+                                                    final int _initialOutboxSize, final int _initialLocalQueueSize) {
+        return null; //todo new NonBlockingReactorMtImpl(_parentReactor, _initialOutboxSize, _initialLocalQueueSize);
+    }
+
+    @Override
+    public ReactorImpl createBlockingReactorImpl(final NonBlockingReactor _parentReactor,
+                                                 final int _initialOutboxSize, final int _initialLocalQueueSize) {
+        return null; //todo new BlockingReactorMtImpl(_parentReactor, _initialOutboxSize, _initialLocalQueueSize);
+    }
+
+    @Override
+    public ReactorImpl createIsolationReactorImpl(final NonBlockingReactor _parentReactor,
+                                                  final int _initialOutboxSize, final int _initialLocalQueueSize) {
+        return null; //todo new IsolationReactorMtImpl(_parentReactor, _initialOutboxSize, _initialLocalQueueSize);
+    }
+
+    @Override
+    public ReactorImpl createSwingBoundReactorImpl(final NonBlockingReactor _parentReactor,
+                                                   final int _initialOutboxSize, final int _initialLocalQueueSize) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ReactorImpl createThreadBoundReactorImpl(final NonBlockingReactor _parentReactor,
+                                                    final int _initialOutboxSize, final int _initialLocalQueueSize,
+                                                    final Runnable _boundProcessor) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <RESPONSE_TYPE> RequestImpl<RESPONSE_TYPE> createSyncRequestImpl(SyncRequest<RESPONSE_TYPE> _syncRequest, Reactor _targetReactor) {
+        return null; //todo new SyncRequestMtImpl<RESPONSE_TYPE>(_syncRequest, _targetReactor);
+    }
+
+    @Override
+    public <RESPONSE_TYPE> AsyncRequestImpl<RESPONSE_TYPE> createAsyncRequestImpl(AsyncRequest<RESPONSE_TYPE> _asyncRequest, Reactor _targetReactor) {
+        return null; //todo new AsyncRequestMtImpl<RESPONSE_TYPE>(_asyncRequest, _targetReactor);
+    }
+
+    /**
+     * Close the Plant.
+     */
+    @Override
+    public void close() throws Exception {
+        if (getSingleton() == null) {
+            return;
+        }
+        try {
+            getInternalReactor().close();
+        } finally {
+            getPlantScheduler().close();
+            super.close();
+        }
+    }
+
+    /**
+     * Returns the Plant's configuration.
+     *
+     * @return The singleton's configuration.
+     */
+    public PlantConfiguration getPlantConfiguration() {
+        return plantConfiguration;
+    }
+
+    /**
+     * Return the scheduler that is a part of the Plant's configuration.
+     *
+     * @return The scheduler.
+     */
+    public PlantScheduler getPlantScheduler() {
+        return plantConfiguration.getPlantScheduler();
+    }
+
+    /**
+     * Create the Plant's internal reactor.
+     *
+     * @return The reactor belonging to the singleton.
+     */
+    protected NonBlockingReactor createInternalReactor() {
+        return new NonBlockingReactor(null);
+    }
+
+    /**
+     * Returns the Runnable which polls for timed out messages.
+     *
+     * @return The Runnable which will perform the poll.
+     */
+    private Runnable plantPoll() {
+        return new Runnable() {
+            public void run() {
+                try {
+                    getInternalReactor().asReactorImpl().reactorPoll();
+                } catch (Exception x) {
+                    x.printStackTrace();
+                }
+            }
+        };
+    }
+
+    /**
+     * Returns the Plant's internal reactor.
+     *
+     * @return The reactor belonging to the singleton.
+     */
+    @Override
+    public NonBlockingReactor getInternalReactor() {
+        return internalReactor;
+    }
+
+    /**
+     * Returns 16.
+     *
+     * @return The reactor default initial local message queue size.
+     */
+    @Override
+    public int getInitialLocalMessageQueueSize() {
+        return 0;
+    }
+
+    /**
+     * Returns 16.
+     *
+     * @return The reactor default initial buffer size.
+     */
+    @Override
+    public int getInitialBufferSize() {
+        return 0;
+    }
+}
