98d80382af2d4006ee775db7b9ee62faf0a39931
Bill La Forge <laforge49@gmail.com>
2014-04-16 07:47:45 +0530

    #178 fix log

--- jactor2-coreSt/src/test/java/org/agilewiki/jactor2/core/impl/plant/LogTest.java
+++ jactor2-coreSt/src/test/java/org/agilewiki/jactor2/core/impl/plant/LogTest.java
@@ -1,12 +1,12 @@
 package org.agilewiki.jactor2.core.impl.plant;
 
 import junit.framework.TestCase;
-import org.agilewiki.jactor2.core.impl.Plant;
 import org.agilewiki.jactor2.core.impl.TestPlantConfiguration;
+import org.agilewiki.jactor2.core.impl.stPlant.PlantConfiguration;
 
 public class LogTest extends TestCase {
     public void testa() throws Exception {
-        TestPlantConfiguration config = new TestPlantConfiguration();
+        PlantConfiguration config = new TestPlantConfiguration();
         config.warn("exception test", new SecurityException("Ignore this"));
     }
 }
