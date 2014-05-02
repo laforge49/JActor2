269590e1e0d1d4a21fa683c1b801ec63c427f785
Bill La Forge <laforge49@gmail.com>
2014-05-02 20:19:52 +0530

    reordered evals

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/BladeMonad.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/BladeMonad.java
@@ -3,49 +3,87 @@ package org.agilewiki.jactor2.core.blades;
 import org.agilewiki.jactor2.core.reactors.Reactor;
 import org.agilewiki.jactor2.core.requests.AsyncRequest;
 import org.agilewiki.jactor2.core.requests.AsyncResponseProcessor;
-import org.agilewiki.jactor2.core.requests.SyncRequest;
 
 public class BladeMonad<Immutable> extends BladeBase {
     private Immutable immutable = null;
+    private BladeMonad<Immutable> parent;
+    private BladeMonad<Immutable> root;
+    protected MonadicFunction<Immutable> function;
+    protected MonadicTransform<Immutable> transform;
+
+    public BladeMonad(final Reactor _reactor) {
+        _initialize(_reactor);
+        root = this;
+    }
+
+    public BladeMonad(final Reactor _reactor, final Immutable _immutable) {
+        _initialize(_reactor);
+        immutable = _immutable;
+        root = this;
+    }
+
+    public BladeMonad(final MonadicFunction<Immutable> _function, BladeMonad<Immutable> _parent) {
+        _initialize(_parent.getReactor());
+        function = _function;
+        parent = _parent;
+        root = parent.root;
+    }
+
+    public BladeMonad(final MonadicTransform<Immutable> _transform, BladeMonad<Immutable> _parent) {
+        _initialize(_parent.getReactor());
+        transform = _transform;
+        parent = _parent;
+        root = parent.root;
+    }
 
     public Immutable getImmutable(final Reactor _sourceReactor) {
         directCheck(_sourceReactor);
         return immutable;
     }
 
-    public BladeMonad<Immutable> bind(final MonadicFunction<Immutable> mf) {
-        return mf.f(this);
+    public BladeMonad<Immutable> bind(final MonadicFunction<Immutable> _function) {
+        return new BladeMonad(_function, this);
     }
 
-    protected void transform(final Immutable _immutable) {
-        immutable = _immutable;
+    public BladeMonad<Immutable> bind(final MonadicTransform<Immutable> _transform) {
+        return new BladeMonad(_transform, this);
     }
 
-    public SyncRequest<Immutable> evalSReq(final BladeMonad<Immutable> _bladeMonad) {
-        return new SyncBladeRequest<Immutable>() {
-            @Override
-            public Immutable processSyncRequest() throws Exception {
-                transform(_bladeMonad.getImmutable(getSourceReactor()));
-                return immutable;
-            }
-        };
-    }
-
-    public AsyncRequest<Immutable> evalAReq(final BladeMonad<Immutable> _bladeMonad) {
+    public AsyncRequest<Immutable> evalAReq() {
         return new AsyncBladeRequest<Immutable>() {
             AsyncRequest<Immutable> dis = this;
+
+            private AsyncResponseProcessor<Immutable> transformResponseProcessor = new AsyncResponseProcessor<Immutable>() {
+                @Override
+                public void processAsyncResponse(Immutable _response) throws Exception {
+                    root.immutable = _response;
+                    dis.processAsyncResponse(_response);
+                }
+            };
 
             private AsyncResponseProcessor<Immutable> evalResponseProcessor = new AsyncResponseProcessor<Immutable>() {
                 @Override
                 public void processAsyncResponse(Immutable _response) throws Exception {
-                    transform(_response);
-                    dis.processAsyncResponse(immutable);
+                    if (transform != null) {
+                        transform.t(_response, transformResponseProcessor);
+                    } else if (function != null) {
+                        immutable = function.f(_response);
+                        root.immutable = _response;
+                        dis.processAsyncResponse(_response);
+                    } else {
+                        root.immutable = _response;
+                        dis.processAsyncResponse(_response);
+                    }
                 }
             };
 
             @Override
             public void processAsyncRequest() throws Exception {
-                send(_bladeMonad.evalAReq(BladeMonad.this), evalResponseProcessor);
+                if (parent == null) {
+                    dis.processAsyncResponse(immutable);
+                    return;
+                }
+                send(parent.evalAReq(), evalResponseProcessor);
             }
         };
     }
--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/MonadicTransform.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/MonadicTransform.java
@@ -3,5 +3,5 @@ package org.agilewiki.jactor2.core.blades;
 import org.agilewiki.jactor2.core.requests.AsyncResponseProcessor;
 
 public interface MonadicTransform<Immutable> {
-    void t(BladeMonad<Immutable> bladeMonad, AsyncResponseProcessor<Immutable> asyncResponseProcessor);
+    void t(Immutable immutable, AsyncResponseProcessor<Immutable> asyncResponseProcessor);
 }
