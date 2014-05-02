7ee2bd70fa242c14fbe403438c143aa88ee28dd2
Bill La Forge <laforge49@gmail.com>
2014-05-02 14:46:04 +0530

    BladeMonad is now more easily extended

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/BladeMonad.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/BladeMonad.java
@@ -8,7 +8,7 @@ import org.agilewiki.jactor2.core.requests.SyncRequest;
 public class BladeMonad<Immutable> extends BladeBase {
     private Immutable immutable = null;
 
-    public Immutable eval(final Reactor _sourceReactor) {
+    public Immutable getImmutable(final Reactor _sourceReactor) {
         directCheck(_sourceReactor);
         return immutable;
     }
@@ -17,11 +17,15 @@ public class BladeMonad<Immutable> extends BladeBase {
         return mf.f(this);
     }
 
+    protected void transform(final Immutable _immutable) {
+        immutable = _immutable;
+    }
+
     public SyncRequest<Immutable> evalSReq(final BladeMonad<Immutable> _bladeMonad) {
         return new SyncBladeRequest<Immutable>() {
             @Override
             public Immutable processSyncRequest() throws Exception {
-                immutable = _bladeMonad.eval(getSourceReactor());
+                transform(_bladeMonad.getImmutable(getSourceReactor()));
                 return immutable;
             }
         };
@@ -31,10 +35,10 @@ public class BladeMonad<Immutable> extends BladeBase {
         return new AsyncBladeRequest<Immutable>() {
             AsyncRequest<Immutable> dis = this;
 
-            AsyncResponseProcessor<Immutable> evalResponseProcessor = new AsyncResponseProcessor<Immutable>() {
+            private AsyncResponseProcessor<Immutable> evalResponseProcessor = new AsyncResponseProcessor<Immutable>() {
                 @Override
                 public void processAsyncResponse(Immutable _response) throws Exception {
-                    immutable = _response;
+                    transform(_response);
                     dis.processAsyncResponse(immutable);
                 }
             };
