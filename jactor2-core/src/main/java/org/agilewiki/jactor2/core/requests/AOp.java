3da55c1e1a66bede4ab9046555e76068322245db
Bill La Forge <laforge49@gmail.com>
2014-06-26 09:35:25 -0400

    reformat

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/requests/AOp.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/requests/AOp.java
@@ -17,8 +17,8 @@ public abstract class AOp<RESPONSE_TYPE> implements AsyncOperation<RESPONSE_TYPE
     /**
      * Create an asynchronous operation.
      *
-     * @param _opName        The name of the operation.
-     * @param _targetReactor The reactor whose thread will process the operation.
+     * @param _opName           The name of the operation.
+     * @param _targetReactor    The reactor whose thread will process the operation.
      */
     public AOp(final String _opName, final Reactor _targetReactor) {
         opName = _opName;
@@ -32,7 +32,7 @@ public abstract class AOp<RESPONSE_TYPE> implements AsyncOperation<RESPONSE_TYPE
      * @param _asyncResponseProcessor Handles the response.
      */
     abstract protected void processAsyncOperation(final AsyncRequest _asyncRequest,
-                                                  final AsyncResponseProcessor<RESPONSE_TYPE> _asyncResponseProcessor)
+                                                final AsyncResponseProcessor<RESPONSE_TYPE> _asyncResponseProcessor)
             throws Exception;
 
     @Override
