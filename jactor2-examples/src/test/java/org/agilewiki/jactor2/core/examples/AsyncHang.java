5f794b4403cca435f454ea3c4b0c12fc137ee059
Bill La Forge <laforge49@gmail.com>
2014-07-11 16:25:17 -0400

    #195 AsyncHang updated

--- jactor2-examples/src/test/java/org/agilewiki/jactor2/core/examples/AsyncHang.java
+++ jactor2-examples/src/test/java/org/agilewiki/jactor2/core/examples/AsyncHang.java
@@ -2,7 +2,10 @@ package org.agilewiki.jactor2.core.examples;
 
 import org.agilewiki.jactor2.core.blades.NonBlockingBladeBase;
 import org.agilewiki.jactor2.core.impl.Plant;
+import org.agilewiki.jactor2.core.requests.AOp;
 import org.agilewiki.jactor2.core.requests.AsyncRequest;
+import org.agilewiki.jactor2.core.requests.AsyncResponseProcessor;
+import org.agilewiki.jactor2.core.requests.impl.AsyncRequestImpl;
 
 public class AsyncHang extends NonBlockingBladeBase {
 
@@ -10,7 +13,7 @@ public class AsyncHang extends NonBlockingBladeBase {
         new Plant();
         try {
             AsyncHang asyncHang = new AsyncHang();
-            asyncHang.hangAReq().call();
+            asyncHang.hangAOp().call();
         } finally {
             Plant.close();
         }
@@ -19,10 +22,10 @@ public class AsyncHang extends NonBlockingBladeBase {
     public AsyncHang() throws Exception {
     }
 
-    public AsyncRequest<Void> hangAReq() {
-        return new AsyncBladeRequest<Void>() {
+    public AOp<Void> hangAOp() {
+        return new AOp<Void>("hang", getReactor()) {
             @Override
-            public void processAsyncRequest() throws Exception {
+            public void processAsyncOperation(AsyncRequestImpl _asyncRequestImpl, AsyncResponseProcessor<Void> _asyncResponseProcessor) throws Exception {
                 //no response--the request hangs
             }
         };
