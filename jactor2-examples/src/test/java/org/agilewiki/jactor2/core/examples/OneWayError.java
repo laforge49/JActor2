97d7d8db6d8b84b44fb119a1d95bddc5937703d7
Bill La Forge <laforge49@gmail.com>
2014-07-12 10:04:47 -0400

    dropped strange import

--- jactor2-examples/src/test/java/org/agilewiki/jactor2/core/examples/OneWayError.java
+++ jactor2-examples/src/test/java/org/agilewiki/jactor2/core/examples/OneWayError.java
@@ -1,6 +1,5 @@
 package org.agilewiki.jactor2.core.examples;
 
-import com.sun.javafx.runtime.async.AbstractAsyncOperation;
 import org.agilewiki.jactor2.core.blades.NonBlockingBladeBase;
 import org.agilewiki.jactor2.core.impl.Plant;
 import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
