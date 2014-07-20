4da0a4de5881e41f6e19bbd9ea3338d5bef2f63e
Bill La Forge <laforge49@gmail.com>
2014-07-20 16:05:19 -0400

    dinigh philosopher

--- docs/tutorials/core/java7/DiningPhilosopher.java
+++ docs/tutorials/core/java7/DiningPhilosopher.java
@@ -1,43 +1,45 @@
 import org.agilewiki.jactor2.core.blades.NonBlockingBladeBase;
-import org.agilewiki.jactor2.core.requests.AsyncRequest;
+import org.agilewiki.jactor2.core.requests.AOp;
 import org.agilewiki.jactor2.core.requests.AsyncResponseProcessor;
-import org.agilewiki.jactor2.core.requests.SyncRequest;
+import org.agilewiki.jactor2.core.requests.SOp;
+import org.agilewiki.jactor2.core.requests.impl.AsyncRequestImpl;
 
 public class DiningPhilosopher extends NonBlockingBladeBase {
 	public DiningPhilosopher() throws Exception {
 	}
 	
-    public AsyncRequest<Integer> feastAReq(final DiningTable _diningTable, final int _seat) {
-        return new AsyncBladeRequest<Integer>() {
-            final AsyncRequest<Integer> dis = this;
-            
+    public AOp<Integer> feastAOp(final DiningTable _diningTable, final int _seat) {
+        return new AOp<Integer>("feast", getReactor()) {
             private int mealsEaten;
             private AsyncResponseProcessor<Void> ateResponseProcessor;
             private AsyncResponseProcessor<Boolean> eatResponseProcessor;
             
             @Override
-            public void processAsyncRequest() {
+            public void processAsyncOperation(final AsyncRequestImpl _asyncRequestImpl, 
+					final AsyncResponseProcessor<Void> _asyncResponseProcessor) throws Exception {
                 ateResponseProcessor = new AsyncResponseProcessor<Void>() {
                     @Override
                     public void processAsyncResponse(final Void _ateResponse) {
-                        AsyncRequest<Boolean> eatAReq = _diningTable.eatAReq(_seat);
-                        send(eatAReq, eatResponseProcessor);
+                        AsyncRequest<Boolean> eatAOp = _diningTable.eatAOp(_seat);
+                        _asyncRequestImpl.send(eatAOp, eatResponseProcessor);
                     }
                 };
+				
                 eatResponseProcessor = new AsyncResponseProcessor<Boolean>() {
                     @Override
                     public void processAsyncResponse(final Boolean _eatResponse) {
                         if (!_eatResponse) {
-                            dis.processAsyncResponse(mealsEaten);
+                            _asyncResponseProcessor.processAsyncResponse(mealsEaten);
                             return;
                         }
                         mealsEaten++;
-                        SyncRequest<Void> ateSReq = _diningTable.ateSReq(_seat);
-                        send(ateSReq, ateResponseProcessor);
+                        SOp<Void> ateSReq = _diningTable.ateSOp(_seat);
+                        _asyncRequestImpl.send(ateSReq, ateResponseProcessor);
                     }
                 };
-                AsyncRequest<Boolean> eatAReq = _diningTable.eatAReq(_seat);
-                send(eatAReq, eatResponseProcessor);
+				
+                AOp<Boolean> eatAOp = _diningTable.eatAOp(_seat);
+                _asyncRequestImpl.send(eatAOp, eatResponseProcessor);
             }
         };
     }
