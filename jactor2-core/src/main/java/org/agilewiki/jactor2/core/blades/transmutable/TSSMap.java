613cc80b1d21ed19d65a63664a88f3d24ce42e21
Bill La Forge <laforge49@gmail.com>
2014-10-02 16:15:03 +0530

    #215 TSSMap

---
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/transmutable/TSSMap.java
@@ -0,0 +1,32 @@
+package org.agilewiki.jactor2.core.blades.transmutable;
+
+import java.util.Map;
+import java.util.SortedMap;
+
+/**
+ * Transmutable Sorted String Map has keys that are strings.
+ */
+public class TSSMap<VALUE> extends TransmutableMap<String, VALUE> {
+    /**
+     * Make an immutable subMap.
+     *
+     * @param map        A sorted map.
+     * @param prefix     The keys in the submap all start with this prefix.
+     * @param <VALUE>    The type of value
+     * @return A subMap.
+     */
+    public static <VALUE> SortedMap<String, VALUE> subMap(SortedMap<String, VALUE> map, String prefix) {
+        map = map.tailMap(prefix);
+        return map.headMap(prefix + Character.MAX_VALUE);
+    }
+
+    /**
+     * Make an immutable subMap.
+     *
+     * @param prefix The keys in the submap all start with this prefix.
+     * @return A subMap.
+     */
+    public SortedMap<String, VALUE> subMap(String prefix) {
+        return subMap((SortedMap<String, VALUE>)this, prefix);
+    }
+}
