a9785b39844a28c9bf654ec73addbe55f3a46b81
Bill La Forge <laforge49@gmail.com>
2014-10-03 12:17:29 +0530

    #215 TSSMSource

---
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/transmutable/tssmTransactions/TSSMSource.java
@@ -0,0 +1,11 @@
+package org.agilewiki.jactor2.core.blades.transmutable.tssmTransactions;
+
+import org.agilewiki.jactor2.core.blades.transmutable.transactions.TransmutableSource;
+
+import java.util.SortedMap;
+
+/**
+ * A TSSMap source.
+ */
+abstract public class TSSMSource<VALUE> extends TransmutableSource<SortedMap<String, VALUE>, TSSMap<VALUE>> {
+}
