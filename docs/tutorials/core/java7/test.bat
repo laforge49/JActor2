ba22a3fb9765f347efe9bd502a2ca6c39c7f422b
Bill La Forge <laforge49@gmail.com>
2014-11-10 12:31:06 +0530

    #230 updated jar files, scripts and getting started page

--- docs/tutorials/core/java7/test.bat
+++ docs/tutorials/core/java7/test.bat
@@ -1,3 +1,3 @@
 del *.class
-javac -classpath jactor2-core-0.13.0.jar;jactor2-coreMt-0.13.0.jar;slf4j-api-1.7.7.jar *.java
-java -classpath jactor2-core-0.13.0.jar;jactor2-coreMt-0.13.0.jar;slf4j-api-1.7.7.jar;slf4j-simple-1.7.7.jar;guava-15.0.jar;metrics-core-3.0.1.jar;. %1
+javac -classpath jactor2-core-1.0.0.jar;jactor2-coreMt-1.0.0.jar;slf4j-api-1.7.7.jar *.java
+java -classpath jactor2-core-1.0.0.jar;jactor2-coreMt-1.0.0.jar;slf4j-api-1.7.7.jar;slf4j-simple-1.7.7.jar;guava-15.0.jar;metrics-core-3.0.1.jar;. %1
