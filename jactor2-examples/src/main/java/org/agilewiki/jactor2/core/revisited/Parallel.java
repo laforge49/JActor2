e57f4343abcd820b4e971e3b9d2ea1cf7abc5ba0
Bill La Forge <laforge49@gmail.com>
2014-12-09 16:12:19 +0530

    #235 Parallel

---
+++ jactor2-examples/src/main/java/org/agilewiki/jactor2/core/revisited/Parallel.java
@@ -0,0 +1,38 @@
+package org.agilewiki.jactor2.core.revisited;
+
+import org.agilewiki.jactor2.core.blades.IsolationBladeBase;
+import org.agilewiki.jactor2.core.impl.Plant;
+import org.agilewiki.jactor2.core.requests.AsyncResponseProcessor;
+import org.agilewiki.jactor2.core.requests.impl.AsyncRequestImpl;
+
+public class Parallel extends IsolationBladeBase {
+
+    public static void main(final String[] args) throws Exception {
+        new Plant();
+        new Parallel(5);
+        System.out.println("initialized");
+    }
+
+    public Parallel(final int _p) throws Exception {
+        new ASig("run") {
+            @Override
+            protected void processAsyncOperation(final AsyncRequestImpl _asyncRequestImpl,
+                        final AsyncResponseProcessor<Void> _asyncResponseProcessor)
+                    throws Exception {
+                AsyncResponseProcessor<Void> runResponseProcessor =
+                        new AsyncResponseProcessor<Void>() {
+                    @Override
+                    public void processAsyncResponse(Void _response) throws Exception {
+                        if (_asyncRequestImpl.hasNoPendingResponses()) {
+                            Plant.close();
+                            System.out.println("finished");
+                        }
+                    }
+                };
+                for (int i = 0; i < _p; i++)
+                    _asyncRequestImpl.send(new Worker(i).run(100000000L, -1),
+                            runResponseProcessor);
+            }
+        }.signal();
+    }
+}
