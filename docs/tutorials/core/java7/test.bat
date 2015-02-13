1f83f5cb8e02e8e8e7db919593cd3801ada9e11e
Bill La Forge <laforge49@gmail.com>
2015-02-13 10:57:55 -0500

    updated to version 1.0.0

--- docs/tutorials/core/java7/test.bat
+++ docs/tutorials/core/java7/test.bat
@@ -1,3 +1,3 @@
 del *.class
-javac -classpath jactor2-core-14.0.0.jar;jactor2-coreMt-14.0.0.jar;slf4j-api-1.7.7.jar *.java
-java -classpath jactor2-core-14.0.0.jar;jactor2-coreMt-14.0.0.jar;slf4j-api-1.7.7.jar;slf4j-simple-1.7.7.jar;guava-18.0.jar;metrics-core-3.1.0.jar;. %1
+javac -classpath jactor2-core-1.0.0.jar;jactor2-coreMt-1.0.0.jar;slf4j-api-1.7.7.jar *.java
+java -classpath jactor2-core-1.0.0.jar;jactor2-coreMt-1.0.0.jar;slf4j-api-1.7.7.jar;slf4j-simple-1.7.7.jar;guava-18.0.jar;metrics-core-3.1.0.jar;. %1
--- docs/tutorials/core/java7/test.sh
+++ docs/tutorials/core/java7/test.sh
@@ -1,3 +1,3 @@
 rm -fr *.class
-javac -cp jactor2-core-14.0.0.jar:jactor2-coreMt-14.0.0.jar:slf4j-api-1.7.7.jar *.java
-java -cp jactor2-core-14.0.0.jar:jactor2-coreMt-14.0.0.jar:slf4j-api-1.7.7.jar:slf4j-simple-1.7.7.jar:guava-18.0.jar:metrics-core-3.1.0.jar:. $1
+javac -cp jactor2-core-1.0.0.jar:jactor2-coreMt-1.0.0.jar:slf4j-api-1.7.7.jar *.java
+java -cp jactor2-core-1.0.0.jar:jactor2-coreMt-1.0.0.jar:slf4j-api-1.7.7.jar:slf4j-simple-1.7.7.jar:guava-18.0.jar:metrics-core-3.1.0.jar:. $1
