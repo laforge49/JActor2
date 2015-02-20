c3eabea3dd13e0755b11519f29f5ecfa25b0ecf6
Bill La Forge <laforge49@gmail.com>
2015-02-20 15:25:45 -0500

    #223 plant package javadocs

---
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/plant/impl/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The classes in this package are implemented in the mtPlant package.
+ */
+package org.agilewiki.jactor2.core.plant.impl;
--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/plant/package-info.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/plant/package-info.java
@@ -1,23 +1,9 @@
 /**
  * <p>
- * Plant is a singleton that supports one or more reactors (beta).
- * Plant takes its name from <i>manufacturing plant</i>, with the reactors performing the application processing.
- * A plant consists of a configuration, a recovery strategy, a scheduler, a thread pool and an internal reactor.
+ * The Plant class is in the jactor2-coreMT module, as it deals with implementation and configuration rather than API.
  * </p>
- * <h3>Related Tutorial Pages</h3>
- * <ul>
- * <li>
- * <a href="../../../../../../tutorials/core/gettingStarted.html" target="_top">Getting Started</a>
- * -- See system properties Debug and Configuration
- * </li>
- * <li>
- * <a href="../../../../../../tutorials/core/recovery.html" target="_top">Recovery</a>
- * - When failures occur
- * </li>
- * <li>
- * <a href="../../../../../../tutorials/core/delays.html" target="_top">Delays</a>
- * - Scalable Timers
- * </li>
- * </ul>
+ * <p>
+ *     The DelayAOp operation is included here because it exposes the functionality of the PlantScheduler.
+ * </p>
  */
 package org.agilewiki.jactor2.core.plant;
