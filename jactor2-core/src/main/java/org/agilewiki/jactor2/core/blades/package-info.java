4d1382f32e11500a945044b41059065cefa01f5b
Bill La Forge <laforge49@gmail.com>
2015-02-23 09:10:39 -0500

    #223 some blade package javadocs

--- jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/package-info.java
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/package-info.java
@@ -1,42 +1,15 @@
 /**
  * <p>
- * A blade is an object with state operated on by requests
- * within the thread context of a single reactor (beta).
+ *     Blades hold state and a set of operations which are performed on that state.
+ * A blade is initialized when it has been assigned a reactor.
+ * (Any number of blades can share the same reactor.)
  * </p>
- * <h3>Related Tutorial Pages</h3>
- * <ul>
- * <li>
- * <a href="../../../../../../tutorials/core/ponger.html" target="_top">Ponger</a>
- * - SyncRequest
- * </li>
- * <li>
- * <a href="../../../../../../tutorials/core/foreignPing.html" target="_top">ForeignPing</a>
- * - AsyncRequest
- * </li>
- * <li>
- * <a href="../../../../../../tutorials/core/pinger.html" target="_top">Pinger</a>
- * - Asynchronous Loops
- * </li>
- * <li>
- * <a href="../../../../../../tutorials/core/batcher.html" target="_top">Batcher</a>
- * - Batched Messages per Second
- * </li>
- * <li>
- * <a href="../../../../../../tutorials/core/parallel.html" target="_top">Parallel</a>
- * - Parallel Processing
- * </li>
- * <li>
- * <a href="../../../../../../tutorials/core/delays.html" target="_top">Delays</a>
- * - Scalable Timers
- * </li>
- * <li>
- * <a href="../../../../../../tutorials/core/dining.html" target="_top">DiningPhilosophers</a>
- * - Pending Responses
- * </li>
- * <li>
- * <a href="../../../../../../tutorials/core/signals.html" target="_top">Signals</a>
- * - One-way Messages
- * </li>
- * </ul>
+ * <p>
+ *     BladeBase provides a number of useful methods, but is not required for an object to be a blade.
+ * </p>
+ * <p>
+ *     There are a number of specialized blades, depending on the type of reactor they use. On the other
+ *     hand, NamedBlade is a blade that has a name and can be registered with a facility.
+ * </p>
  */
 package org.agilewiki.jactor2.core.blades;
---
+++ jactor2-core/src/main/java/org/agilewiki/jactor2/core/blades/transmutable/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <p>
+ *     A transmutable is a modifyable object which implements the Transmutable interface and which
+ *     can be converted to an unmodifiable form and back again.
+ * </p>
+ */
+package org.agilewiki.jactor2.core.blades.transmutable;
